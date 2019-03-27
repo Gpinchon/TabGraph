fe05939b984768d96220206f9558d0066495df06
gpinchon <gpinchon@student.42.fr>
2019-03-27 14:08:20 +0100

    terrain WIP

---
+++ tests/terrain/main.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpinchon <gpinchon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/18 20:44:09 by gpinchon          #+#    #+#             */
+/*   Updated: 2019/03/03 16:46:25 by gpinchon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define USE_HIGH_PERFORMANCE_GPU
+#include "DLLExport.hpp"
+
+#include "scop.hpp"
+#include "Config.hpp"
+#include "Window.hpp"
+#include "Engine.hpp"
+#include "Camera.hpp"
+#include "Events.hpp"
+#include "GameController.hpp"
+#include "Keyboard.hpp"
+#include "Mouse.hpp"
+#include "parser/OBJ.hpp"
+#include "parser/HDR.hpp"
+#include "parser/FBX.hpp"
+#include "parser/GLSL.hpp"
+#include "Material.hpp"
+#include "Render.hpp"
+#include "Light.hpp"
+#include "CubeMesh.hpp"
+#include "Debug.hpp"
+#include "ComputeObject.hpp"
+#include "Vgroup.hpp"
+#include "Terrain.hpp"
+#include <iostream>
+#include <unistd.h>
+#include <iostream>
+#include <csignal>
+
+int		main(int /*argc*/, char **/*argv*/)
+{
+	Config::Load(Engine::resource_path() + "config.ini");
+	Engine::init();
+	auto camera = OrbitCamera::create("main_camera", 45, M_PI / 2.f, M_PI / 2.f, 5.f);
+	Camera::set_current(camera);
+	camera->set_target(Node::create("main_camera_target", new_vec3(0, 0, 0), new_vec3(0, 0, 0), new_vec3(1, 1, 1)));
+	camera->orbite(M_PI / 2.f, M_PI / 2.f, 5.f);
+
+	Terrain::create("terrain_test", new_vec2(1024, 1024), new_vec3(100, 100, 1000), Texture::parse("heightMap", "res/heightMap.png"));
+	
+	//FBX::parseBin(Engine::program_path() + "./mug.fbx");
+	setup_callbacks();
+	//create_random_lights(250);
+	DirectionnalLight::create("MainLight", new_vec3(1, 1, 1), new_vec3(10, 10, 10), 1, true);
+	//DirectionnalLight::create("BackLight", new_vec3(0.3, 0.3, 0.3), new_vec3(-10, -10, 0), 1, false);
+	Engine::run();
+	SDL_Quit();
+	return (0);
+}
---
+++ tests/terrain/scop.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scop.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpinchon <gpinchon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/18 20:44:18 by gpinchon          #+#    #+#             */
+/*   Updated: 2018/09/24 19:26:20 by gpinchon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Engine.hpp"
+
+const auto WIDTH = 1280;
+const auto HEIGHT = 720;
+
+/*
+** Key binding
+*/
+
+#define DOWNK SDL_SCANCODE_DOWN
+#define UPK SDL_SCANCODE_UP
+#define LEFTK SDL_SCANCODE_LEFT
+#define RIGHTK SDL_SCANCODE_RIGHT
+#define ZOOMK SDL_SCANCODE_KP_PLUS
+#define UNZOOMK SDL_SCANCODE_KP_MINUS
+
+//bool			load_mtllib(const std::string &path);
+
+/*
+** Callback functions
+*/
+void setup_callbacks();
+void callback_refresh(SDL_Event* event);
+void callback_camera(SDL_Event* event);
+void callback_scale(SDL_KeyboardEvent* event);
+void callback_background(SDL_KeyboardEvent* event);
+void callback_exit(SDL_KeyboardEvent* event);
+void callback_fullscreen(SDL_KeyboardEvent* event);
+void callback_quality(SDL_KeyboardEvent* event);
+void controller_callback_background(SDL_ControllerButtonEvent* event);
+void controller_callback_quality(SDL_ControllerButtonEvent* event);
+void controller_callback_rotation(SDL_ControllerButtonEvent* event);
+void keyboard_callback_rotation(SDL_KeyboardEvent* event);
+void MouseWheelCallback(SDL_MouseWheelEvent* event);
+void MouseMoveCallback(SDL_MouseMotionEvent* event);
