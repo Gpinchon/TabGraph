257163f0ebbeeb72d56cef38018f4720dddae25a
gpinchon <gpinchon@student.42.fr>
2019-06-04 14:13:08 +0200

    Add documentation to Engine.hpp

--- include/Engine.hpp
+++ include/Engine.hpp
@@ -2,7 +2,7 @@
 * @Author: gpi
 * @Date:   2019-02-22 16:19:03
 * @Last Modified by:   gpi
-* @Last Modified time: 2019-05-21 13:54:26
+* @Last Modified time: 2019-06-04 14:08:59
 */
 
 #pragma once
@@ -22,16 +22,30 @@ namespace std
 /** @brief This class orchestrates the rendering loop and logics around it
 */
 namespace Engine {
+    /** @brief Initializes the Engine */
     void    Init(void);
+    /** @brief Starts Engine loop */
     void    Start(void);
+    /** @brief Tells the Engine to stop the loop */
     void    Stop(void);
-    void    SetSwapInterval(int8_t);
-
+    /** @brief Sets the Swap Interval
+        @arg inerval : See https://wiki.libsdl.org/SDL_GL_SetSwapInterval
+    */
+    void    SetSwapInterval(int8_t interval);
+    /** @brief Returns the current Swap Interval
+        @return See https://wiki.libsdl.org/SDL_GL_SetSwapInterval
+    */
     int8_t  SwapInterval(void);
+    /** @return Timing between the last two Updates */
     double  DeltaTime(void);
+    /** @return Timing between the last two Fixed Updates, mainly for event, 0.016 or more */
     double  FixedDeltaTime(void);
+    /** @return See http://manpagesfr.free.fr/man/man3/getcwd.3.html */
     const std::string&  ExecutionPath(void);
+    /** @return See https://wiki.libsdl.org/SDL_GetBasePath */
     const std::string&  ProgramPath(void);
+    /** @return ProgramPath + "/res/" */
     const std::string   ResourcePath(void);
+    /** @brief Lock this mutex to keep Engine from performing next update, usefull in multithreaded applications */
     std::mutex          &UpdateMutex(void);
 };
