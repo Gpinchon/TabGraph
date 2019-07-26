d5749eb65620a41385813e1b510e92832663b110
gpinchon <gpinchon@student.42.fr>
2019-07-26 14:52:51 +0200

    refactor Config

--- include/Config.hpp
+++ include/Config.hpp
@@ -8,10 +8,37 @@
 #pragma once
 
 #include "glm/glm.hpp" // for glm::vec2
+#include <map>
 #include <stdint.h> // for uint16_t, int16_t
 #include <string> // for allocator, string
 #include <variant>
-#include <map>
+
+/**
+* @brief Loader for config files
+* @summary The Config file might contains values formated as such :
+* WindowSize = 1024 768
+* WindowName = "Window Name"
+*/
+class ConfigFile
+{
+public:
+    /**
+    * Loads the Config from the specified file
+    * Invalid values will be ignored (set to default value) but setting keys will still be registered
+    */
+    void Parse(const std::string &path);
+    /** Saves the Config to the specified file */
+    void Save(const std::string &path);
+    /** Tries to get the specified setting, set it to the default value if not found */
+    template <typename T>
+    T Get(const std::string &name, const T defaultValue);
+    /** Sets the specified setting to the specified value */
+    template <typename T>
+    T Set(const std::string &name, const T value);
+
+private:
+    std::map<std::string, std::variant<float, glm::vec2, glm::vec3, std::string>> _configMap;
+};
 
 /**
 * @brief Loads/Saves a config file
@@ -20,86 +47,64 @@
 * WindowSize = 1024 768
 * WindowName = "Window Name"
 */
-class Config final {
+class Config final
+{
 public:
     /**
         * Loads the Config from the specified file
         * Invalid values will be ignored (set to default value) but setting keys will still be registered
         */
-    static void Parse(const std::string& path);
+    static void Parse(const std::string &path);
     /** Saves the Config to the specified file */
-    static void Save(const std::string& path);
+    static void Save(const std::string &path);
     /** Tries to get the specified setting, set it to the default value if not found */
     template <typename T>
-    static T Get(const std::string& name, const T defaultValue);
+    static T Get(const std::string &name, const T defaultValue);
     /** Sets the specified setting to the specified value */
     template <typename T>
-    static T Set(const std::string& name, const T value);
-private :
-    static Config& _instance();
-    std::map<std::string, std::variant<float, glm::vec2, glm::vec3, std::string>> _configMap;
+    static T Set(const std::string &name, const T value);
+
+private:
+    static ConfigFile &_instance();
+    //std::map<std::string, std::variant<float, glm::vec2, glm::vec3, std::string>> _configMap;
 };
 
 template <>
-inline int Config::Get<int>(const std::string& name, const int defaultValue)
+inline int ConfigFile::Get<int>(const std::string &name, const int defaultValue)
 {
     return Get<float>(name, defaultValue);
 }
 
 template <>
-inline unsigned Config::Get<unsigned>(const std::string& name, const unsigned defaultValue)
+inline unsigned ConfigFile::Get<unsigned>(const std::string &name, const unsigned defaultValue)
 {
     return Get<float>(name, defaultValue);
 }
 
 template <typename T>
-inline T Config::Get(const std::string& name, const T defaultValue)
+inline T ConfigFile::Get(const std::string &name, const T defaultValue)
 {
-    auto it = _instance()._configMap.find(name);
-    if (it != _instance()._configMap.end())
+    auto it = _configMap.find(name);
+    if (it != _configMap.end())
         return std::get<T>((*it).second);
     else
-        return Config::Set(name, defaultValue);
+        return ConfigFile::Set(name, defaultValue);
 }
 
 template <typename T>
-inline T Config::Set(const std::string& name, const T value)
+inline T ConfigFile::Set(const std::string &name, const T value)
 {
-    return std::get<T>(_instance()._configMap[name] = value);
+    return std::get<T>(_configMap[name] = value);
 }
 
+template <typename T>
+inline T Config::Get(const std::string &name, const T defaultValue)
+{
+    return _instance().Get<T>(name, defaultValue);
+}
 
-/*class Config {
-public:
-    static void Load(const std::string&);
-    static glm::ivec2& WindowSize();
-    static std::string& WindowName();
-    static float& Anisotropy();
-    static int16_t& MaxTexRes();
-    static uint16_t& ShadowRes();
-    static uint16_t& Msaa();
-    static uint16_t& BloomPass();
-    static uint16_t& LightsPerPass();
-    static uint16_t& ShadowsPerPass();
-    static uint16_t& ReflexionSteps();
-    static uint16_t& ReflexionSamples();
-    static uint16_t& ReflexionBorderFactor();
-
-private:
-    static Config* _get();
-    static Config* _instance;
-    glm::ivec2 _windowSize { 1280, 720 };
-    std::string _windowName { "" };
-    float _anisotropy { 16.f };
-    int16_t _maxTexRes { 0 };
-    uint16_t _shadowRes { 2048 };
-    uint16_t _msaa { 0 };
-    uint16_t _bloomPass { 1 };
-    uint16_t _lightsPerPass { 32 };
-    uint16_t _shadowsPerPass { 16 };
-    uint16_t _ReflexionSteps { 10 };
-    uint16_t _reflexionSamples { 9 };
-    uint16_t _reflexionBorderFactor { 10 };
-    Config() = default;
-    ~Config() = default;
-};*/
+template <typename T>
+inline T Config::Set(const std::string &name, const T value)
+{
+    return _instance().Set(name, value);
+}
--- src/Config.cpp
+++ src/Config.cpp
@@ -14,49 +14,49 @@
 #include <stdio.h> // for sscanf, fgets, fopen
 #include <vector>
 
-Config& Config::_instance()
-{
-    static Config* instance = nullptr;
-    if (instance == nullptr)
-        instance = new Config;
-    return *instance;
-}
-
-void Config::Parse(const std::string& path)
+void ConfigFile::Parse(const std::string &path)
 {
     std::ifstream configFile(path);
-    for (std::string line; std::getline(configFile, line);) {
+    for (std::string line; std::getline(configFile, line);)
+    {
         std::istringstream iss(line);
         std::vector<std::string> words(
-            std::istream_iterator<std::string> { iss },
+            std::istream_iterator<std::string>{iss},
             std::istream_iterator<std::string>());
-        if (words.size() > 2 && words.at(1) == "=") {
-            switch (words.size() - 2) {
+        if (words.size() > 2 && words.at(1) == "=")
+        {
+            switch (words.size() - 2)
+            {
             case 1:
-                try {
-                    _instance()._configMap[words.at(0)] = std::stof(words.at(2));
-                } catch (std::exception&) {
-                    _instance()._configMap[words.at(0)] = words.at(2);
+                try
+                {
+                    _configMap[words.at(0)] = std::stof(words.at(2));
+                }
+                catch (std::exception &)
+                {
+                    _configMap[words.at(0)] = words.at(2);
                 }
                 break;
             case 2:
-                _instance()._configMap[words.at(0)] = glm::vec2(std::stof(words.at(2)), std::stof(words.at(3)));
+                _configMap[words.at(0)] = glm::vec2(std::stof(words.at(2)), std::stof(words.at(3)));
                 break;
             case 3:
-                _instance()._configMap[words.at(0)] = glm::vec3(std::stof(words.at(2)), std::stof(words.at(3)), std::stof(words.at(4)));
+                _configMap[words.at(0)] = glm::vec3(std::stof(words.at(2)), std::stof(words.at(3)), std::stof(words.at(4)));
                 break;
             }
         }
     }
 }
 
-void Config::Save(const std::string& path)
+void ConfigFile::Save(const std::string &path)
 {
     std::ofstream configFile;
     configFile.open(path);
-    for (const auto& v : _instance()._configMap) {
+    for (const auto &v : _configMap)
+    {
         configFile << v.first << " = ";
-        switch (v.second.index()) {
+        switch (v.second.index())
+        {
         case 0:
             configFile << std::get<0>(v.second);
             break;
@@ -74,114 +74,20 @@ void Config::Save(const std::string& path)
     }
 }
 
-/*
-Config* Config::_instance = nullptr;
-
-Config* Config::_get()
+ConfigFile &Config::_instance()
 {
-    if (nullptr == _instance)
-        _instance = new Config();
-    return (_instance);
+    static ConfigFile *instance = nullptr;
+    if (instance == nullptr)
+        instance = new ConfigFile;
+    return *instance;
 }
 
-void Config::Load(const std::string& path)
+void Config::Parse(const std::string &path)
 {
-    auto fd = fopen(path.c_str(), "r");
-    if (nullptr == fd) {
-        debugLog("Config file not found");
-        return;
-    }
-    char buffer[4096];
-    while (fgets(buffer, 4096, fd)) {
-        unsigned valu, valu1;
-        int vali;
-        char vals[4096];
-        if (sscanf(buffer, "WindowName = %s", vals)) {
-            Config::WindowName() = vals;
-        } else if (sscanf(buffer, "WindowSize = %u %u", &valu, &valu1)) {
-            Config::WindowSize().x = valu;
-            Config::WindowSize().y = valu1;
-        } else if (sscanf(buffer, "MaxTexRes = %u", &vali)) {
-            Config::MaxTexRes() = vali;
-        } else if (sscanf(buffer, "ShadowRes = %u", &valu)) {
-            Config::ShadowRes() = valu;
-        } else if (sscanf(buffer, "Anisotropy = %u", &valu)) {
-            Config::Anisotropy() = valu;
-        } else if (sscanf(buffer, "MSAA = %u", &valu)) {
-            Config::Msaa() = valu;
-        } else if (sscanf(buffer, "BloomPass = %u", &valu)) {
-            Config::BloomPass() = valu;
-        } else if (sscanf(buffer, "LightsPerPass = %u", &valu)) {
-            Config::LightsPerPass() = valu;
-        } else if (sscanf(buffer, "ShadowsPerPass = %u", &valu)) {
-            Config::ShadowsPerPass() = valu;
-        } else if (sscanf(buffer, "ReflexionSteps = %u", &valu)) {
-            Config::ReflexionSteps() = valu;
-        } else if (sscanf(buffer, "ReflexionSamples = %u", &valu)) {
-            Config::ReflexionSamples() = valu;
-        } else if (sscanf(buffer, "ReflexionBorderFactor = %u", &valu)) {
-            Config::ReflexionBorderFactor() = valu;
-        }
-    }
+    _instance().Parse(path);
 }
 
-glm::ivec2& Config::WindowSize()
+void Config::Save(const std::string &path)
 {
-    return (_get()->_windowSize);
+    _instance().Save(path);
 }
-
-std::string& Config::WindowName()
-{
-    return (_get()->_windowName);
-}
-
-float& Config::Anisotropy()
-{
-    return (_get()->_anisotropy);
-}
-
-int16_t& Config::MaxTexRes()
-{
-    return (_get()->_maxTexRes);
-}
-
-uint16_t& Config::ShadowRes()
-{
-    return (_get()->_shadowRes);
-}
-
-uint16_t& Config::Msaa()
-{
-    return (_get()->_msaa);
-}
-
-uint16_t& Config::BloomPass()
-{
-    return (_get()->_bloomPass);
-}
-
-uint16_t& Config::LightsPerPass()
-{
-    return (_get()->_lightsPerPass);
-}
-
-uint16_t& Config::ShadowsPerPass()
-{
-    return (_get()->_shadowsPerPass);
-}
-
-uint16_t& Config::ReflexionSteps()
-{
-    return (_get()->_ReflexionSteps);
-}
-
-uint16_t& Config::ReflexionSamples()
-{
-    return (_get()->_reflexionSamples);
-}
-
-uint16_t& Config::ReflexionBorderFactor()
-{
-    return (_get()->_reflexionBorderFactor);
-}
-*/
