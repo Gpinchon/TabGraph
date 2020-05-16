cca7a1bd32ed27fd35a059cb1a2e346e4f8fce89
gpinchon <gpinchon@student.42.fr>
2020-05-16 14:57:23 +0200

    move Transform code to its own cpp for now

--- include/Transform.hpp
+++ include/Transform.hpp
@@ -15,91 +15,53 @@
 class Transform : public Object
 {
 public:
-    static std::shared_ptr<Transform> Create(const std::string &name) { return std::shared_ptr<Transform>(new Transform(name));  };
-    std::shared_ptr<Transform> shared_from_this() { return std::static_pointer_cast<Transform>(Object::shared_from_this()); };
-    inline glm::mat4 WorldTransformMatrix() const {
-        auto worldTransformMatrix(Parent() ? Parent()->WorldTransformMatrix() * LocalTransformMatrix() : LocalTransformMatrix());
-        //std::cout << "================================================================================" << std::endl;
-        //std::cout << "LocalTranslationMatrix " << Name() << " " << glm::to_string(LocalTranslationMatrix()) << std::endl;
-        //std::cout << "LocalRotationMatrix    " << Name() << " " << glm::to_string(LocalRotationMatrix()) << std::endl;
-        //std::cout << "LocalScaleMatrix       " << Name() << " " << glm::to_string(LocalScaleMatrix()) << std::endl;
-        //std::cout << "LocalTransformMatrix   " << Name() << " " << glm::to_string(LocalTransformMatrix()) << std::endl;
-        //std::cout << "worldTransformMatrix   " << Name() << " " << glm::to_string(worldTransformMatrix) << std::endl;
-        //std::cout << "================================================================================" << std::endl;
-        return worldTransformMatrix;
-    };
-    inline glm::mat4 WorldTranslationMatrix() const { return Parent() ? Parent()->WorldTransformMatrix() * LocalTranslationMatrix() : LocalTranslationMatrix(); };
-    inline glm::mat4 WorldRotationMatrix() const { return Parent() ? Parent()->WorldTransformMatrix() * LocalRotationMatrix() : LocalRotationMatrix(); };
-    inline glm::mat4 WorldScaleMatrix() const { return Parent() ? Parent()->WorldTransformMatrix() * LocalScaleMatrix() : LocalScaleMatrix(); };
-    inline glm::mat4 LocalTransformMatrix() const { return LocalTranslationMatrix() * LocalRotationMatrix() * LocalScaleMatrix(); };
-    inline glm::mat4 LocalTranslationMatrix() const { return glm::translate(Position()); };
-    inline glm::mat4 LocalRotationMatrix() const { return glm::mat4_cast(Rotation()); };
-    inline glm::mat4 LocalScaleMatrix() const { return glm::scale(Scale()); };
-    inline void SetLocalTransform(glm::mat4 transform) {
-        glm::vec3 skew;
-        glm::vec4 perspective;
-        glm::decompose(transform, _scale, _rotation, _position, skew, perspective);
-        //glm::vec3 scale;
-        //glm::quat rotation;
-        //glm::vec3 translation;
-        //glm::vec3 skew;
-        //glm::vec4 perspective;
-        //glm::decompose(transform, scale, rotation, translation, skew, perspective);
-        //SetPosition(translation);
-        //SetRotation(glm::normalize(rotation));
-        //SetScale(scale);
-    };
-    inline glm::vec3 WorldPosition() const { return Parent() ? Parent()->WorldTransformMatrix() * glm::vec4(Position(), 1.f) : Position(); }
-    inline glm::quat WorldRotation() const { return Parent() ? Parent()->WorldTransformMatrix() * glm::mat4_cast(Rotation()) : Rotation(); }
-    inline glm::vec3 WorldScale() const { return Parent() ? Parent()->WorldTransformMatrix() * glm::vec4(Scale(), 1.f) : Scale(); }
+    static std::shared_ptr<Transform> Create(const std::string &name);
+    std::shared_ptr<Transform> shared_from_this();
+    glm::mat4 WorldTransformMatrix();
+    glm::mat4 WorldTranslationMatrix() const;
+    glm::mat4 WorldRotationMatrix() const;
+    glm::mat4 WorldScaleMatrix() const;
+    glm::mat4 LocalTransformMatrix();
+    glm::mat4 LocalTranslationMatrix() const;
+    glm::mat4 LocalRotationMatrix() const;
+    glm::mat4 LocalScaleMatrix() const;
+    void SetLocalTransform(glm::mat4 transform);
+    glm::vec3 WorldPosition() const;
+    glm::quat WorldRotation() const;
+    glm::vec3 WorldScale() const;
 	/** @return the node local position */
-    inline glm::vec3 Position() const { return _position; }
+    glm::vec3 Position() const;
     /** @argument position : the node local position */
-    inline void SetPosition(glm::vec3 position) { _position = position; };
+    void SetPosition(glm::vec3 position);
     /** @return the node local rotation */
-    inline glm::quat Rotation() const { return _rotation; };
+    glm::quat Rotation() const;
     /** @argument rotation : the node local rotation */
-    inline void SetRotation(glm::vec3 rotation) { _rotation = glm::quat(rotation); };
+    void SetRotation(glm::vec3 rotation);
     /** @return the node local scale */
-    inline void SetRotation(glm::quat rotation) { _rotation = rotation; };
+    void SetRotation(glm::quat rotation);
     /** @return the node local scale */
-    inline glm::vec3 Scale() const { return _scale; };
+    glm::vec3 Scale() const;
     /** @argument scale : the node local scale */
-    inline void SetScale(glm::vec3 scale) { _scale = scale; };
+    void SetScale(glm::vec3 scale);
     /**
      * @brief Common::Forward() * Rotation()
      * READONLY : Computed on demand
      */
-    inline glm::vec3 Forward() const { return Rotation() * Common::Forward(); };
+    glm::vec3 Forward() const;
     /**
      * @brief READONLY : Computed on demand
      * @return Common::Up() * Rotation()
      */
-    inline glm::vec3 Up() const { return Rotation() * Common::Up(); };
+    glm::vec3 Up() const;
     /**
      * @brief READONLY : Computed on demand
      * Common::Right() * Rotation()
      */
-    inline glm::vec3 Right() const { return Rotation() * Common::Right(); };
-    inline void LookAt(const glm::vec3 &target, const glm::vec3 &up = Common::Up()) {
-        auto direction(normalize(target - WorldPosition()));
-        SetRotation(glm::quatLookAt(direction, up));
-    };
-    inline void LookAt(const std::shared_ptr<Transform> &target, const glm::vec3 &up = Common::Up()) {
-        if (target == nullptr)
-            return;
-        LookAt(target->WorldPosition(), up);
-    };
-    inline std::shared_ptr<Transform> Parent() const { return _parent; }
-    inline void SetParent(std::shared_ptr<Transform> parent) {
-        if (parent == shared_from_this() || Parent() == parent)
-            return;
-        //if (Parent() != nullptr)
-        //    Parent()->RemoveChild(shared_from_this());
-        _parent = parent;
-        //if (parent != nullptr)
-        //    parent->AddChild(shared_from_this());
-    }
+    glm::vec3 Right() const;
+    void LookAt(const glm::vec3 &target, const glm::vec3 &up = Common::Up());
+    void LookAt(const std::shared_ptr<Transform> &target, const glm::vec3 &up = Common::Up());
+    std::shared_ptr<Transform> Parent() const;
+    void SetParent(std::shared_ptr<Transform> parent);
     ~Transform() = default;
 
 private:
@@ -116,6 +78,8 @@ private:
     std::shared_ptr<Transform> _parent;
     //std::vector<std::shared_ptr<Transform>> _children;
 	glm::vec3 _position { 0, 0, 0 };
-    glm::quat _rotation /*{ 0, 0, 0, 1 }*/;
+    glm::quat _rotation { 0, 0, 0, 1 };
     glm::vec3 _scale { 1, 1, 1 };
+    bool _needsUpdate { true };
+    glm::mat4 _localTransformMatrix;
 };
---
+++ src/Transform.cpp
@@ -0,0 +1,189 @@
+#include "Transform.hpp"
+
+std::shared_ptr<Transform> Transform::Create(const std::string &name)
+{
+    return std::shared_ptr<Transform>(new Transform(name));
+}
+
+std::shared_ptr<Transform> Transform::shared_from_this()
+{
+    return std::static_pointer_cast<Transform>(Object::shared_from_this());
+}
+
+glm::mat4 Transform::WorldTransformMatrix()
+{
+    auto worldTransformMatrix(Parent() ? Parent()->WorldTransformMatrix() * LocalTransformMatrix() : LocalTransformMatrix());
+    //std::cout << "================================================================================" << std::endl;
+    //std::cout << "LocalTranslationMatrix " << Name() << " " << glm::to_string(LocalTranslationMatrix()) << std::endl;
+    //std::cout << "LocalRotationMatrix    " << Name() << " " << glm::to_string(LocalRotationMatrix()) << std::endl;
+    //std::cout << "LocalScaleMatrix       " << Name() << " " << glm::to_string(LocalScaleMatrix()) << std::endl;
+    //std::cout << "LocalTransformMatrix   " << Name() << " " << glm::to_string(LocalTransformMatrix()) << std::endl;
+    //std::cout << "worldTransformMatrix   " << Name() << " " << glm::to_string(worldTransformMatrix) << std::endl;
+    //std::cout << "================================================================================" << std::endl;
+    return worldTransformMatrix;
+}
+
+glm::mat4 Transform::WorldTranslationMatrix() const
+{
+    return Parent() ? Parent()->WorldTransformMatrix() * LocalTranslationMatrix() : LocalTranslationMatrix();
+}
+
+glm::mat4 Transform::WorldRotationMatrix() const
+{
+    return Parent() ? Parent()->WorldTransformMatrix() * LocalRotationMatrix() : LocalRotationMatrix();
+}
+
+glm::mat4 Transform::WorldScaleMatrix() const
+{
+    return Parent() ? Parent()->WorldTransformMatrix() * LocalScaleMatrix() : LocalScaleMatrix();
+}
+
+glm::mat4 Transform::LocalTransformMatrix()
+{
+    if (_needsUpdate)
+        _localTransformMatrix = LocalTranslationMatrix() * LocalRotationMatrix() * LocalScaleMatrix();
+    return _localTransformMatrix;
+}
+
+glm::mat4 Transform::LocalTranslationMatrix() const
+{
+    return glm::translate(Position());
+}
+
+glm::mat4 Transform::LocalRotationMatrix() const
+{
+    return glm::mat4_cast(Rotation());
+}
+
+glm::mat4 Transform::LocalScaleMatrix() const
+{
+    return glm::scale(Scale());
+}
+
+void Transform::SetLocalTransform(glm::mat4 transform) {
+    glm::vec3 scale;
+    glm::quat rotation;
+    glm::vec3 translation;
+    glm::vec3 skew;
+    glm::vec4 perspective;
+    glm::decompose(transform, scale, rotation, translation, skew, perspective);
+    SetPosition(translation);
+    SetRotation(rotation);
+    SetScale(scale);
+}
+
+glm::vec3 Transform::WorldPosition() const
+{
+    return Parent() ? Parent()->WorldTransformMatrix() * glm::vec4(Position(), 1.f) : Position();
+}
+
+glm::quat Transform::WorldRotation() const
+{
+    return Parent() ? Parent()->WorldTransformMatrix() * glm::mat4_cast(Rotation()) : Rotation();
+}
+
+glm::vec3 Transform::WorldScale() const
+{
+    return Parent() ? Parent()->WorldTransformMatrix() * glm::vec4(Scale(), 1.f) : Scale();
+}
+
+/** @return the node local position */
+glm::vec3 Transform::Position() const
+{
+    return _position;
+}
+
+/** @argument position : the node local position */
+void Transform::SetPosition(glm::vec3 position)
+{
+    _needsUpdate |= _position != position;
+    _position = position;
+}
+
+/** @return the node local rotation */
+glm::quat Transform::Rotation() const
+{
+    return _rotation;
+}
+
+/** @argument rotation : the node local rotation */
+void Transform::SetRotation(glm::vec3 rotation)
+{
+    _rotation = glm::quat(rotation);
+}
+
+/** @return the node local scale */
+void Transform::SetRotation(glm::quat rotation)
+{
+    _needsUpdate |= _rotation != rotation;
+    _rotation = rotation;
+}
+
+/** @return the node local scale */
+glm::vec3 Transform::Scale() const
+{
+    return _scale;
+}
+
+/** @argument scale : the node local scale */
+void Transform::SetScale(glm::vec3 scale)
+{
+    _needsUpdate |= _scale != scale;
+    _scale = scale;
+}
+
+/**
+ * @brief Common::Forward() * Rotation()
+ * READONLY : Computed on demand
+ */
+glm::vec3 Transform::Forward() const
+{
+    return Rotation() * Common::Forward();
+}
+
+/**
+ * @brief READONLY : Computed on demand
+ * @return Common::Up() * Rotation()
+ */
+glm::vec3 Transform::Up() const
+{
+    return Rotation() * Common::Up();
+}
+
+/**
+ * @brief READONLY : Computed on demand
+ * Common::Right() * Rotation()
+ */
+glm::vec3 Transform::Right() const
+{
+    return Rotation() * Common::Right();
+}
+
+void Transform::LookAt(const glm::vec3 &target, const glm::vec3 &up)
+{
+    auto direction(normalize(target - WorldPosition()));
+    SetRotation(glm::quatLookAt(direction, up));
+}
+
+void Transform::LookAt(const std::shared_ptr<Transform> &target, const glm::vec3 &up)
+{
+    if (target == nullptr)
+        return;
+    LookAt(target->WorldPosition(), up);
+}
+
+std::shared_ptr<Transform> Transform::Parent() const
+{
+    return _parent;
+}
+
+void Transform::SetParent(std::shared_ptr<Transform> parent)
+{
+    if (parent == shared_from_this() || Parent() == parent)
+        return;
+    //if (Parent() != nullptr)
+    //    Parent()->RemoveChild(shared_from_this());
+    _parent = parent;
+    //if (parent != nullptr)
+    //    parent->AddChild(shared_from_this());
+}
