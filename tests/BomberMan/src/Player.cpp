6c2630ff3371c6ab03d6ca0a2840b5dab0eaf789
gpinchon <gpinchon@student.42.fr>
2020-09-01 15:27:40 +0200

    fix Bomberman Player collision

--- tests/BomberMan/src/Player.cpp
+++ tests/BomberMan/src/Player.cpp
@@ -71,17 +71,32 @@ struct Contact {
     float penetration;
 };
 
-auto AABBvsCircle(Contact& c, const glm::vec2& sphereCenter, float sphereRadius, const glm::vec2& boxPos, const glm::vec2& boxMin, const glm::vec2& boxMax)
+auto AABBvsCircle(Contact& c, const glm::vec2& sphereCenter, float sphereRadius, const glm::vec2& boxPos, const glm::vec2& boxSize)
 {
-    glm::vec2 aabb_half_extents((boxMax - boxMin) / 2.f);
+    /*glm::vec2 aabbSize((boxMax - boxMin) / 2.f);
+    glm::vec2 nearest = glm::max(boxPos - aabbSize, glm::min(sphereCenter, boxPos + aabbSize));
+    glm::vec2 delta = sphereCenter - nearest;
+    c.normal = -normalize(delta);
+    c.penetration = sphereRadius - length(delta);
+    return (delta.x * delta.x + delta.y * delta.y) < (sphereRadius * sphereRadius);*/
+    //auto NearestX = glm::max(boxPos.x, glm::min(sphereCenter.x, boxPos.x + aabbSize.x));
+    //auto NearestY = glm::max(boxPos.y, glm::min(sphereCenter.y, boxPos.y + aabbSize.y));
+    /*auto DeltaX = sphereCenter.x - NearestX;
+    auto DeltaY = sphereCenter.y - NearestY;
+    return (DeltaX * DeltaX + DeltaY * DeltaY) < (sphereRadius * sphereRadius);*/
+    //glm::vec2 aabb_half_extents(0.5f);
     auto C = sphereCenter;
     auto B = boxPos;
     auto BC = C - B; //D
-    auto P = B + glm::clamp(BC, -aabb_half_extents, aabb_half_extents);
+    auto P = B + glm::clamp(BC, -boxSize, boxSize);
     auto CP = P - C;
     auto CPLen = length(CP);
-    if (CPLen <= sphereRadius) {
-        c.normal = CP / CPLen;
+    auto CPdBC = dot(CP, BC);
+    if (CPLen < sphereRadius) {
+        if (CPLen == 0.f)
+            c.normal = -normalize(BC);
+        else
+            c.normal = CP / CPLen;
         c.penetration = sphereRadius - CPLen;
         return true;
     }
@@ -115,80 +130,43 @@ void Player::_FixedUpdateCPU(float delta)
     glm::vec2 input;
     input.x = Keyboard::key(UPK) - Keyboard::key(DOWNK);
     input.y = Keyboard::key(RIGHTK) - Keyboard::key(LEFTK);
-    if (length(input) > 0.f) {
+    auto inputLength = length(input);
+    if (inputLength > 0.f) {
+        input /= inputLength;
         auto cameraT = Game::CurrentLevel()->CurrentCamera()->GetComponent<Transform>();
         auto cameraPosition = glm::vec2(cameraT->WorldPosition().x, cameraT->WorldPosition().z);
         auto cameraForward = normalize(glm::vec2(cameraT->Forward().x, cameraT->Forward().z));
         auto projPlayerPosition = ProjectPointOnPlane(Position(), cameraPosition, cameraForward);
         auto forward = normalize(Position() - projPlayerPosition);
-        auto right = glm::vec2(cameraT->Right().x, cameraT->Right().z);
+        auto right = normalize(glm::vec2(cameraT->Right().x, cameraT->Right().z));
         Move(input * (forward + right), delta);
     }
-    auto maxX = glm::clamp(int(Position().x + 1), 0, Game::CurrentLevel()->Size().x - 1);
-    auto minX = glm::clamp(int(Position().x - 1), 0, Game::CurrentLevel()->Size().x - 1);
-    auto maxY = glm::clamp(int(Position().y + 1), 0, Game::CurrentLevel()->Size().y - 1);
-    auto minY = glm::clamp(int(Position().y - 1), 0, Game::CurrentLevel()->Size().y - 1);
-    bool foundNewDir = false;
-    for (auto x = minX; x <= maxX; ++x) {
-        for (auto y = minY; y <= maxY; ++y) {
-            auto entity = Game::CurrentLevel()->GetGameEntity(glm::ivec2(x, y));
-            if (entity == nullptr) {
-                continue;
-            }
-            if (x == int(Position().x) && y == int(Position().y) && entity->Type() == "Bomb")
-                continue;
-            Contact contact;
-            auto intersects = AABBvsCircle(contact, Position(), 0.4f, entity->Position(), entity->Position() - 0.5f, entity->Position() + 0.5f);
-            if (intersects) {
-                std::cout << entity->Type() << std::endl;
-                std::cout << "contact.normal      : " << contact.normal.x << ' ' << contact.normal.y << std::endl;
-                std::cout << "contact.penetration : " << contact.penetration << std::endl;
-                if (entity->Type() == "Flame") {
-                    Die();
+    bool collides;
+    do {
+        collides = false;
+        auto maxX = glm::clamp(int(Position().x + 1), 0, Game::CurrentLevel()->Size().x - 1);
+        auto minX = glm::clamp(int(Position().x - 1), 0, Game::CurrentLevel()->Size().x - 1);
+        auto maxY = glm::clamp(int(Position().y + 1), 0, Game::CurrentLevel()->Size().y - 1);
+        auto minY = glm::clamp(int(Position().y - 1), 0, Game::CurrentLevel()->Size().y - 1);
+        for (auto x = minX; x <= maxX; ++x) {
+            for (auto y = minY; y <= maxY; ++y) {
+                auto entity = Game::CurrentLevel()->GetGameEntity(glm::ivec2(x, y));
+                if (entity == nullptr) {
                     continue;
                 }
-                SetPosition(Position() - contact.normal * std::max(contact.penetration, 0.001f));
-                x = 0;
-                break;
-                //a->NextTransform().SetPosition(glm::vec3(newPos.x, Height(), newPos.y));
-                /*auto P = glm::vec2(out.Position().x, out.Position().z);
-                auto N = normalize(glm::vec2(out.Normal().x, out.Normal().z));
-                auto V = dir;
-                auto A = glm::vec2(a->NextTransform().WorldPosition().x, a->NextTransform().WorldPosition().z);
-                auto B = ProjectPointOnPlane(A, P, N);
-                auto AB = B - A;
-                auto ABLen = length(AB);
-                auto alpha = glm::acos(dot(V, AB / ABLen));
-                auto BCLen = tan(alpha) * ABLen;
-                auto ACLen = ABLen * ABLen + BCLen * BCLen;
-                auto C = A + V * ACLen;
-                auto BC = C - B;
-                std::cout << "alpha: " << alpha << '\n';
-                std::cout << "dir  : " << dir.x << ' ' << dir.y << '\n';
-                std::cout << "N    : " << N.x << ' ' << N.y << '\n';
-                std::cout << "P    : " << P.x << ' ' << P.y << '\n';
-                std::cout << "A    : " << A.x << ' ' << A.y << '\n';
-                std::cout << "B    : " << B.x << ' ' << B.y << '\n';
-                std::cout << "C    : " << C.x << ' ' << C.y << '\n';
-                if (glm::isnan(alpha) || glm::degrees(alpha) > 45) {
-                    foundNewDir = true;
-                    dir = BC / BCLen;
-                    x = 0;
-                    break;
-                }*/
-                /*if (glm::isnan(alpha) || glm::degrees(alpha) > 45) {
-                    std::cout << "BC   : " << BC.x << ' ' << BC.y << '\n';
-                    std::cout << "BCLen: " << BCLen << '\n';
-                    std::cout << std::endl;
-                    dir = BC / BCLen;
-                    auto nextPlayerPos = A + (dir * Speed() * delta);
-                    a->NextTransform().SetPosition(glm::vec3(nextPlayerPos.x, Height(), nextPlayerPos.y));
-                    x = 0;
-                    break;
-                }  else
-                    newPlayerPosition = glm::vec2(a->NextTransform().WorldPosition().x, a->NextTransform().WorldPosition().z);*/
-                std::cout << std::endl;
+                if (x == int(Position().x) && y == int(Position().y) && entity->Type() == "Bomb")
+                    continue;
+                Contact contact;
+                auto intersects = AABBvsCircle(contact, Position(), 0.4f, entity->Position(), entity->Size() * 0.5f);
+                if (intersects) {
+                    if (entity->Type() == "Flame") {
+                        Die();
+                        continue;
+                    }
+                    SetPosition(Position() - contact.normal * std::max(contact.penetration, 0.001f));
+                    collides = true;
+                }
             }
         }
-    }
+    } while (collides);
 }
