ac68d58b2b77d5d3f2e598b7a8bba06aec4273c4
gpinchon <gpinchon@student.42.fr>
2020-12-04 20:13:53 +0100

    add copy constructor for Signal

--- TabGraph/include/Event/Signal.hpp
+++ TabGraph/include/Event/Signal.hpp
@@ -16,22 +16,37 @@ public:
 	Signal() = default;
 	~Signal() = default;
 
+	Signal(const Signal &) {}
+
+	/**
+	* @brief Connects a row functor to the signal
+	* @return The slot ID
+	*/
 	auto Connect(std::function<void(Args...)> const& slot) {
 		_slots[++_currentSlotID] = slot;
 		return _currentSlotID;
 	}
 
+	/**
+	* @brief Connects member method using smart ptr, disconnects if inst was destroyed
+	* @argument inst : the class instance the functor will use
+	* @argument func : the functor to connect
+	* @return The slot ID
+	*/
 	template<typename T>
 	auto ConnectMember(std::weak_ptr<T> inst, void (T::* func)(Args...)) {
 		_slots[++_currentSlotID] = ([=](Args... args) {
 			if (inst.lock() != nullptr)
-				(inst.lock())->*func(args...);
+				(inst.lock().get()->*func)(args...);
 			else
 				Disconnect(_currentSlotID);
 			});
 		return _currentSlotID;
 	}
 
+	/**
+	* @brief const overload of ConnectMember
+	*/
 	template<typename T>
 	auto ConnectMember(std::weak_ptr<T> inst, void (T::* func)(Args...) const) {
 		_slots[++_currentSlotID] = ([slotID = _currentSlotID, this](Args... args) {
@@ -39,14 +54,43 @@ public:
 				(inst.lock())->*func(args...);
 			else
 				this->Disconnect(_currentSlotID);
-		});
+			});
 		return _currentSlotID;
 	}
 
+	/**
+	* @brief Connect member using raw ptr, less safe if object gets destroyed
+	* @argument inst : the class instance the functor will use
+	* @argument func : the functor to connect
+	* @return The slot ID
+	*/
+	template<typename T>
+	auto ConnectMember(T* inst, void (T::* func)(Args...)) {
+		return Connect([=](Args... args) {
+			(inst->*func)(args...);
+		});
+	}
+
+	/**
+	* @brief const overload of ConnectMember
+	*/
+	template<typename T>
+	auto ConnectMember(T* inst, void (T::* func)(Args...) const) {
+		return Connect([=](Args... args) {
+			(inst->*func)(args...);
+			});
+	}
+
+	/**
+	* @brief disconnects a slot from the signal
+	*/
 	auto Disconnect(uint32_t slotID) {
 		_slots.erase(slotID);
 	}
 
+	/**
+	* @brief Emits the signal and calls the functors connected to it
+	*/
 	auto Emit(Args... args) const {
 		for (const auto& slot : _slots)
 			slot.second(args...);
