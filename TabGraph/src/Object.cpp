6e355d2d17f8581f66e23078574a885859467041
gpinchon <gpinchon@student.42.fr>
2021-01-27 14:04:46 +0100

    update Object

--- TabGraph/src/Object.cpp
+++ TabGraph/src/Object.cpp
@@ -1,21 +1,21 @@
 /*
-* @Author: gpi
-* @Date:   2019-02-22 16:13:28
+* @Author: gpinchon
+* @Date:   2021-01-08 17:02:47
 * @Last Modified by:   gpinchon
-* @Last Modified time: 2020-06-09 09:56:50
+* @Last Modified time: 2021-01-11 08:41:23
 */
 
 #include "Object.hpp"
 #include "Debug.hpp"
-#include <random>
 #include <limits>
+#include <random>
 
 Object::Object()
 {
     static uint64_t objNbr(0);
     static std::random_device rd;
     static std::mt19937 gen(rd());
-    static std::uniform_int_distribution<int64_t> dis(0, std::numeric_limits<int64_t>::max() );
+    static std::uniform_int_distribution<int64_t> dis(0, std::numeric_limits<int64_t>::max());
     SetId(dis(gen)); //Generate unique ID
     SetName("Object_" + std::to_string(objNbr));
     objNbr++;
@@ -29,25 +29,5 @@ Object::Object(const std::string& name)
 
 Object::~Object()
 {
-    debugLog(Name());
-}
-
-int64_t Object::Id() const
-{
-    return _id;
-}
-
-void Object::SetId(int64_t id)
-{
-    _id = id;
-}
-
-std::string Object::Name() const
-{
-    return (_name);
-}
-
-void Object::SetName(const std::string& name)
-{
-    _name = name;
+    debugLog(GetName());
 }
