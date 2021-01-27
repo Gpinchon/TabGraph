189babd33e9087a666d985b1946e9e23fea4f381
gpinchon <gpinchon@student.42.fr>
2021-01-27 14:06:38 +0100

    update DirectionnalLight

--- TabGraph/src/Light/DirectionnalLight.cpp
+++ TabGraph/src/Light/DirectionnalLight.cpp
@@ -58,10 +58,10 @@ DirectionnalLight::DirectionnalLight(const std::string& name, glm::vec3 color, g
     SetComponent(DirectionnalLightShader());
     SetCastShadow(cast_shadow);
     if (cast_shadow) {
-        SetComponent(Component::Create<Framebuffer>(Name() + "_shadowMap", glm::vec2(Config::Get("ShadowRes", 1024)), 0, 0));
-        GetComponent<Framebuffer>()->Create_attachement(GL_DEPTH_COMPONENT, GL_DEPTH_COMPONENT24);
-        GetComponent<Framebuffer>()->depth()->set_parameteri(GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
-        GetComponent<Framebuffer>()->depth()->set_parameteri(GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+        SetComponent(Component::Create<Framebuffer>(GetName() + "_shadowMap", glm::vec2(Config::Get("ShadowRes", 1024)), 0, 0));
+        GetComponent<Framebuffer>()->Create_attachement(Pixel::SizedFormat::Depth24);
+        GetComponent<Framebuffer>()->depth()->SetParameter(GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
+        GetComponent<Framebuffer>()->depth()->SetParameter(GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
     }
 }
 
