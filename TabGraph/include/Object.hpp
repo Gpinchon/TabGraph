4e1887731ba88318fb95f534a467c96a9429f2f7
gpinchon <gpinchon@student.42.fr>
2021-01-27 14:01:27 +0100

    use Property for Object

--- TabGraph/include/Object.hpp
+++ TabGraph/include/Object.hpp
@@ -1,8 +1,8 @@
 /*
-* @Author: gpi
+* @Author: gpinchon
 * @Date:   2019-02-22 16:19:03
 * @Last Modified by:   gpinchon
-* @Last Modified time: 2020-06-09 10:09:35
+* @Last Modified time: 2021-01-11 08:45:41
 */
 
 #pragma once
@@ -10,8 +10,8 @@
 #include "Event/Signal.hpp"
 
 #define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
 #include <crtdbg.h>
+#include <stdlib.h>
 
 #include <memory> // for enable_shared_from_this, shared_ptr
 #include <string> // for string
@@ -26,32 +26,51 @@
 */
 
 /** Use this to declare a new property */
-#define PROPERTY(type, var, ...)    \
+#define PROPERTY(type, var, ...)   \
+public:                            \
+    Signal<type> var##Changed;     \
+    type Get##var() const          \
+    {                              \
+        return _##var;             \
+    }                              \
+    void Set##var(const type& val) \
+    {                              \
+        if (val != _##var) {       \
+            _##var = val;          \
+            var##Changed(val);     \
+        }                          \
+    }                              \
+                                   \
+private:                           \
+    type _##var { __VA_ARGS__ };
+
+#define READONLYPROPERTY(type, var, ...) \
 public:                             \
     Signal<type> var##Changed;      \
     type Get##var() const           \
     {                               \
         return _##var;              \
     }                               \
-    void Set##var(const type& val)  \
+                                    \
+protected:                          \
+    void _Set##var(const type& val) \
     {                               \
         if (val != _##var) {        \
             _##var = val;           \
             var##Changed(val);      \
         }                           \
     }                               \
-                                    \
 private:                            \
     type _##var { __VA_ARGS__ };
 
 #define PRIVATEPROPERTY(type, var, ...) \
 private:                                \
     Signal<type> var##Changed;          \
-    type Get##var() const               \
+    type _Get##var() const              \
     {                                   \
         return _##var;                  \
     }                                   \
-    void Set##var(const type& val)      \
+    void _Set##var(const type& val)     \
     {                                   \
         bool changed = val != _##var;   \
         _##var = val;                   \
@@ -61,16 +80,11 @@ private:                                \
     type _##var { __VA_ARGS__ };
 
 class Object : public std::enable_shared_from_this<Object>, public Trackable {
+    PROPERTY(int64_t, Id, reinterpret_cast<int64_t>(this));
+    PROPERTY(std::string, Name, "");
+
 public:
     Object();
     Object(const std::string& name);
-    int64_t Id() const;
-    void SetId(int64_t);
-    std::string Name() const;
-    void SetName(const std::string& name);
     virtual ~Object();
-
-private:
-    std::string _name { "" };
-    int64_t _id { reinterpret_cast<int64_t>(this) };
 };
