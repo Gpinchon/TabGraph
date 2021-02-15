e02264ec2d99b719379d296cb5eecd3ec2bc2d86
gpinchon <gpinchon@student.42.fr>
2021-02-15 12:39:04 +0100

    slight Window Refactor

--- TabGraph/include/Window.hpp
+++ TabGraph/include/Window.hpp
@@ -7,34 +7,22 @@
 
 #pragma once
 
-#include "Event/InputDevice.hpp" // for InputDevice
-#include "glm/glm.hpp" // for glm::vec2, glm::vec4, s_vec2
-#include <GL/glew.h> // for GLbitfield
-#include <SDL_events.h> // for SDL_Event
-#include <SDL_video.h> // for SDL_GLContext, SDL_Window
+#include "Property.hpp"
+
+#include <glm/glm.hpp> // for glm::vec2, glm::vec4, s_vec2
 #include <string> // for string
 
-class Window : InputDevice {
-public:
-    static void resize(const glm::ivec2&);
-    static glm::ivec2 size();
-    static void init(const std::string& name, glm::ivec2 resolution);
-    static void fullscreen(const bool& fullscreen);
-    static void swap();
-    static GLbitfield& clear_mask();
-    static glm::vec4& clear_color();
-    //static Framebuffer	&render_buffer();
-    void process_event(SDL_Event*) override;
-    static SDL_GLContext context();
-    static SDL_Window* sdl_window();
-
-private:
-    static Window& _get();
-    static Window* _instance;
-    SDL_Window* _sdl_window { nullptr };
-    SDL_GLContext _gl_context {};
-    glm::vec4 _clear_color { 0, 0, 0, 0 };
-    GLbitfield _clear_mask {};
-    //Framebuffer		*_render_buffer{nullptr};
-    Window();
+namespace Window {
+    /**
+     * @brief May change regarding media backend
+     * May be SDL_Window
+    */
+    typedef void* Handle;
+    
+    void SetSize(const glm::ivec2&);
+    glm::ivec2 GetSize();
+    void Init(const std::string& name, glm::ivec2 resolution);
+    void SetFullscreen(const bool& fullscreen);
+    void Swap();
+    Handle GetHandle();
 };
--- TabGraph/src/Window.cpp
+++ TabGraph/src/Window.cpp
@@ -6,6 +6,8 @@
 */
 
 #include "Window.hpp"
+#include "Event/InputDevice.hpp" // for InputDevice
+
 #include "Config.hpp" // for Config
 #include "Debug.hpp" // for glCheckError
 #include "Engine.hpp" // for Stop
@@ -16,14 +18,16 @@
 #include <SDL_joystick.h> // for SDL_JoystickEventState
 #include <stdexcept> // for runtime_error
 
-Window* Window::_instance = nullptr;
+struct WindowImpl : InputDevice {
+    static WindowImpl& _get();
+    static WindowImpl* _instance;
+    Window::Handle _handle{ nullptr };
+    virtual void process_event(SDL_Event*) override;
+};
 
-Window::Window()
-{
-    Events::Add(this, SDL_WINDOWEVENT);
-}
+WindowImpl* WindowImpl::_instance = nullptr;
 
-void Window::process_event(SDL_Event* event)
+void WindowImpl::process_event(SDL_Event* event)
 {
     if (event->window.event == SDL_WINDOWEVENT_CLOSE) {
         Engine::Stop();
@@ -36,102 +40,56 @@ void Window::process_event(SDL_Event* event)
 ** window is a singleton
 */
 
-Window& Window::_get()
+WindowImpl& WindowImpl::_get()
 {
-    if (_instance == nullptr)
-        _instance = new Window();
+    if (_instance == nullptr) {
+        _instance = new WindowImpl();
+        Events::Add(_instance, SDL_WINDOWEVENT);
+    }
     return (*_instance);
 }
 
-void Window::swap()
+void Window::Swap()
 {
-    SDL_GL_SwapWindow(_get()._sdl_window);
+    SDL_GL_SwapWindow((SDL_Window*)WindowImpl::_get()._handle);
 }
 
-SDL_GLContext Window::context()
+Window::Handle Window::GetHandle()
 {
-    return (_get()._gl_context);
+    return WindowImpl::_get()._handle;
 }
 
-SDL_Window* Window::sdl_window()
+void Window::Init(const std::string& name, glm::ivec2 resolution)
 {
-    return (_get()._sdl_window);
-}
-
-void PrintExtensions()
-{
-    debugLog("GL Extensions :");
-    GLint n;
-    glGetIntegerv(GL_NUM_EXTENSIONS, &n);
-    for (auto i = 0; i < n; ++i) {
-        debugLog(glGetStringi(GL_EXTENSIONS, i));
-    }
-}
-
-void Window::init(const std::string& name, glm::ivec2 resolution)
-{
+    if (GetHandle() != nullptr)
+        return;
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
         throw std::runtime_error(SDL_GetError());
     }
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 0);
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    if (nullptr == _get()._sdl_window)
-        _get()._sdl_window = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED, resolution.x, resolution.y,
-            SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
-    if (_get()._sdl_window == nullptr) {
+    WindowImpl::_get()._handle = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, resolution.x, resolution.y,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
+    if (GetHandle() == nullptr) {
         throw std::runtime_error(SDL_GetError());
     }
-    _get()._gl_context = SDL_GL_CreateContext(_get()._sdl_window);
-    if (_get()._gl_context == nullptr) {
-        throw std::runtime_error(SDL_GetError());
-    }
-    _get()._clear_mask = GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT;
-    glewExperimental = GL_TRUE;
-    auto error = glewInit();
-    if (error != GLEW_OK) {
-        throw std::runtime_error(reinterpret_cast<const char*>(glewGetErrorString(error)));
-    }
-    debugLog(std::string("GL vendor    : ") + reinterpret_cast<const char*>(glGetString(GL_VENDOR)));
-    debugLog(std::string("GL renderer  : ") + reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
-    debugLog(std::string("GL version   : ") + reinterpret_cast<const char*>(glGetString(GL_VERSION)));
-    debugLog(std::string("GLSL version : ") + reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION)));
-    PrintExtensions();
-    glEnable(GL_MULTISAMPLE);
-    glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
-#ifdef DEBUG_MOD
-    // During init, enable debug output
-    glEnable(GL_DEBUG_OUTPUT);
-    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, GL_DONT_CARE, nullptr, GL_FALSE);
-    glDebugMessageCallback(MessageCallback, 0);
-#endif
 }
 
-GLbitfield& Window::clear_mask()
-{
-    return (_get()._clear_mask);
-}
-
-glm::ivec2 Window::size()
+glm::ivec2 Window::GetSize()
 {
     int w;
     int h;
 
-    SDL_GL_GetDrawableSize(_get()._sdl_window, &w, &h);
+    SDL_GL_GetDrawableSize((SDL_Window*)GetHandle(), &w, &h);
     return (glm::vec2(w, h));
 }
 
-void Window::resize(const glm::ivec2& size)
+void Window::SetSize(const glm::ivec2& size)
 {
-    SDL_SetWindowSize(_get()._sdl_window, size.x, size.y);
+    SDL_SetWindowSize((SDL_Window*)GetHandle(), size.x, size.y);
 }
 
-void Window::fullscreen(const bool& fullscreen)
+void Window::SetFullscreen(const bool& fullscreen)
 {
-    SDL_SetWindowFullscreen(_get()._sdl_window,
+    SDL_SetWindowFullscreen((SDL_Window*)GetHandle(),
         fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
 }
