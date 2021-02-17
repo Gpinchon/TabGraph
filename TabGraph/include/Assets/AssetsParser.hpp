51deab63a5339b333a4752752909296691ca1400
gpinchon <gpinchon@student.42.fr>
2021-02-17 23:26:39 +0100

    add AssetsParser documentation

--- TabGraph/include/Assets/AssetsParser.hpp
+++ TabGraph/include/Assets/AssetsParser.hpp
@@ -22,12 +22,28 @@ public:
     using MimeExtensionPair = std::pair<MimeType, FileExtension>;
     /**
      * @brief Returns the MIME type if managed
-     * @param  the file extension to get the MIME type for
-     * @return the corresponding MIME type or "" if not managed
+     * @param extension the file extension to get the MIME type for
+     * @return the corresponding MIME type or an empty string if not managed
     */
-    static MimeType GetMimeFromExtension(const FileExtension&);
-    static AssetsParser* Add(const MimeType&, ParsingFunction);
+    static MimeType GetMimeFromExtension(const FileExtension& extension);
+    /**
+     * @brief Creates a new asset parser and returns a pointer to it, don't free it
+     * @param mime the mime type this parser is for @ref https://en.wikipedia.org/wiki/Media_type
+     * @param function the parsing function that will be used to parse that mime type
+     * @return a pointer to the newly created AssetsParser
+    */
+    static AssetsParser* Add(const MimeType& mime, ParsingFunction function);
+    /**
+     * @brief Adds a new extension for this mime type
+     * @param mime @ref https://en.wikipedia.org/wiki/Media_type
+     * @param extension might be .jpeg, .jpg...
+     * @return a pair of the mime type and the extension
+    */
     static MimeExtensionPair AddMimeExtension(const MimeType& mime, const FileExtension& extension);
+    /**
+     * @brief Parses the specified asset using available parsers, won't do anything if no parser was found
+     * @param asset the Asset to load, mime type is figured out using Asset's Uri
+    */
     static void Parse(std::shared_ptr<Asset> asset);
 
 private:
