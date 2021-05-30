3191c37b48b76a03e9edd2b68318459e31bee7a0
gpinchon <gpinchon@student.42.fr>
2021-05-30 21:29:30 +0200

    use mutex for Signal

--- TabGraph/include/Event/Signal.hpp
+++ TabGraph/include/Event/Signal.hpp
@@ -2,14 +2,14 @@
 * @Author: gpinchon
 * @Date:   2021-01-04 09:42:56
 * @Last Modified by:   gpinchon
-* @Last Modified time: 2021-01-04 20:41:35
+* @Last Modified time: 2021-05-28 18:26:57
 */
 #pragma once
 
+#include <mutex>
 #include <functional>
 #include <memory>
 #include <unordered_map>
-#include <atomic>
 
 /**
 * Inspired by schneegans
@@ -18,12 +18,15 @@
 
 class Trackable {
 public:
-    class TrackablePointee {};
+    class TrackablePointee {
+    };
     Trackable() = default;
-    Trackable(const Trackable&) : _controlBlock{ static_cast<TrackablePointee*>(0) } {};
-    auto GetWeakPtr() { return _controlBlock; }
+    Trackable(const Trackable&)
+        : _controlBlock { static_cast<TrackablePointee*>(0) } {};
+    auto GetWeakPtr() { return std::weak_ptr(_controlBlock); }
+
 private:
-    std::shared_ptr<TrackablePointee> _controlBlock{ static_cast<TrackablePointee*>(0), [](TrackablePointee*) {} };
+    std::shared_ptr<TrackablePointee> _controlBlock { static_cast<TrackablePointee*>(0), [](TrackablePointee*) {} };
 };
 
 template <typename... Args>
@@ -33,7 +36,8 @@ public:
     struct Slot {
         Slot() = default;
         ~Slot() = default;
-        bool Connected() {
+        bool Connected()
+        {
             return !_signalRef.expired();
         }
         void Disconnect()
@@ -42,7 +46,8 @@ public:
                 _signal->_Disconnect(this->_id);
             Reset();
         }
-        SlotID Id() {
+        SlotID Id()
+        {
             return _id;
         }
 
@@ -57,27 +62,27 @@ public:
             _trackedObjectRef.reset();
         }
         Slot(std::function<void(Args...)> func, SlotID id,
-            Signal* signal, Trackable *trackable = nullptr
-            )
+            Signal* signal, Trackable* trackable = nullptr)
             : _func(func)
             , _id(id)
             , _signal(signal)
             , _signalRef(signal->GetWeakPtr())
-            , _trackedObjectRef(trackable ? trackable->GetWeakPtr() : nullptr)
+            , _trackedObjectRef(trackable ? trackable->GetWeakPtr() : signal->GetWeakPtr()) {};
+        auto operator()(Args... args)
         {
-        };
-        auto operator()(Args... args) {
-            if (_func) return _func(args...);
+            if (_func)
+                return _func(args...);
         }
-        std::function<void(Args...)> _func{};
-        SlotID _id{ 0 };
-        Signal* _signal{ nullptr };
+        std::function<void(Args...)> _func {};
+        SlotID _id { 0 };
+        Signal* _signal { nullptr };
         std::weak_ptr<TrackablePointee> _signalRef;
         std::weak_ptr<TrackablePointee> _trackedObjectRef;
     };
     struct ScoppedSlot : public Slot {
         ScoppedSlot() = default;
-        ~ScoppedSlot() {
+        ~ScoppedSlot()
+        {
             Disconnect();
         }
         ScoppedSlot(const ScoppedSlot& other)
@@ -88,22 +93,30 @@ public:
         {
             *this = other;
         }
-        ScoppedSlot& operator=(const ScoppedSlot& other) {
+        ScoppedSlot& operator=(const ScoppedSlot& other)
+        {
             Disconnect();
-            Slot::operator=(other);
+            _func = other._func;
+            _id = other._id;
+            _signal = other._signal;
+            _signalRef = other._signalRef;
+            _trackedObjectRef = other._trackedObjectRef;
             return *this;
         }
-        ScoppedSlot& operator=(const Slot& other) {
+        ScoppedSlot& operator=(const Slot& other)
+        {
             Disconnect();
-            Slot::operator=(other);
+            _func = other._func;
+            _id = other._id;
+            _signal = other._signal;
+            _signalRef = other._signalRef;
+            _trackedObjectRef = other._trackedObjectRef;
             return *this;
         }
     };
 
-    Signal() {
-    };
-    ~Signal() {
-    };
+    Signal() {};
+    ~Signal() {};
     Signal(const Signal&)
         : Signal()
     {
@@ -127,7 +140,7 @@ public:
     * @return The slot ID
     */
     template <typename T>
-    auto ConnectMember(T* inst, void (T::* func)(Args...))
+    auto ConnectMember(T* inst, void (T::*func)(Args...))
     {
         static_assert(std::is_base_of<Trackable, T>::value, "T must inherit Trackable");
         ++_currentSlotID;
@@ -158,10 +171,9 @@ public:
 	*/
     auto operator()(Args... args)
     {
-        while (_inUse) {}
-        _inUse = true;
-        for (auto &slotIt : _slots) {
-            auto& slot{ slotIt.second };
+        std::unique_lock lock(_lock);
+        for (auto& slotIt : _slots) {
+            auto& slot { slotIt.second };
             //if slot is not member method, tracked object is this signal -> never expires until Signal's destruction
             if (slot._trackedObjectRef.expired()) {
                 slot.Disconnect();
@@ -170,14 +182,13 @@ public:
             if (std::find(_toDisconnect.begin(), _toDisconnect.end(), slot._id) == _toDisconnect.end())
                 slot(args...);
         }
-        for (const auto &slot : _toConnect)
+        for (const auto& slot : _toConnect)
             _slots[slot._id] = slot;
         _toConnect.clear();
         //IMPORTANT : If we have disconnected a signal after connecting it while emitting this signal, it should be disconnected at the end
         for (const auto& id : _toDisconnect)
             _slots.erase(id);
         _toDisconnect.clear();
-        _inUse = false;
     }
 
 private:
@@ -187,22 +198,27 @@ private:
     */
     inline auto _Disconnect(SlotID slot)
     {
-        if (_inUse)
-            _toDisconnect.push_back(slot);
-        else
+        if (_lock.try_lock()) {
             _slots.erase(slot);
+            _lock.unlock();
+        }
+        else
+            _toDisconnect.push_back(slot);
     }
 
-    inline auto _Connect(const Slot &slot) {
-        if (_inUse)
-            _toConnect.push_back(slot);
-        else
+    inline auto _Connect(const Slot& slot)
+    {
+        if (_lock.try_lock()) {
             _slots[slot._id] = slot;
+            _lock.unlock();
+        }
+        else
+            _toConnect.push_back(slot);
         return slot;
     }
     std::unordered_map<SlotID, Slot> _slots;
     std::vector<SlotID> _toDisconnect;
     std::vector<Slot> _toConnect;
-    std::atomic<bool> _inUse{ false };
+    std::mutex _lock;
     SlotID _currentSlotID { 0 };
 };
