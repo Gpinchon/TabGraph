f010cfd73e30bd6d44d336391aa6fb78e16b5252
gpinchon <gpinchon@student.42.fr>
2021-06-01 11:47:20 +0200

    add TaskComplete Event

--- TabGraph/include/DispatchQueue.hpp
+++ TabGraph/include/DispatchQueue.hpp
@@ -14,9 +14,15 @@
 #include <functional>
 
 struct DispatchQueue {
+    struct TaskIdentifier {
+        uint64_t dispatchQueueID;
+        uint64_t taskID;
+    };
     DispatchQueue(size_t threadsCount = 1)
         : _threads(threadsCount)
     {
+        static uint64_t s_ID{0};
+        _ID = ++s_ID;
         for (auto& thread : _threads)
             thread = std::thread(&DispatchQueue::_DispatchThreadHandler, this);
     }
@@ -28,34 +34,27 @@ struct DispatchQueue {
             if (thread.joinable())
                 thread.join();
     }
-    void Dispatch(std::function<void(void)> fun)
+    TaskIdentifier Dispatch(std::function<void(void)> fun)
     {
         std::unique_lock<std::mutex> lock(_lock);
-        _queue.push(fun);
+        _queue.push({fun, ++_taskID});
         lock.unlock();
         _cv.notify_one();
+        return { _ID, _taskID };
     }
+    /**
+     * @brief use this to dispatch an event, threadsCount is defined at compilation time
+     * @return the application's DispatchQueue
+    */
+    static DispatchQueue& ApplicationDispatchQueue();
 
 private:
-    void _DispatchThreadHandler(void)
-    {
-        std::unique_lock<std::mutex> lock(_lock);
-        while (_running) {
-            _cv.wait(lock, [this] {
-                return _queue.size() || !_running;
-            });
-            if (_queue.empty())
-                continue;
-            auto fun { std::move(_queue.front()) };
-            _queue.pop();
-            lock.unlock();
-            fun();
-            lock.lock();
-        }
-    }
+    void _DispatchThreadHandler(void);
+    uint64_t _ID{ 0 };
+    uint64_t _taskID { 0 };
     bool _running { true };
     std::vector<std::thread> _threads;
     std::mutex _lock;
     std::condition_variable _cv;
-    std::queue<std::function<void(void)>> _queue;
+    std::queue<std::pair<std::function<void(void)>, uint64_t>> _queue;
 };
--- TabGraph/include/Event/Event.hpp
+++ TabGraph/include/Event/Event.hpp
@@ -89,6 +89,7 @@ struct Event {
         //User event for custom events
         User,
         AssetLoaded,
+        TaskComplete,
         MaxValue
     };
     struct Empty {
@@ -167,6 +168,13 @@ struct Event {
         bool loaded { false };
         std::shared_ptr<::Asset> asset;
     };
+    /**
+     * @brief emitted when a task dispatched through a DispatchQueue gets completed.
+    */
+    struct TaskComplete {
+        uint64_t dispatchQueueID;
+        uint64_t taskID;
+    };
     Type type { Event::Type::Unknown };
     const std::chrono::system_clock::time_point timestamp { std::chrono::system_clock::now() };
     std::variant<
@@ -182,7 +190,8 @@ struct Event {
         Event::GameControllerButton,
         Event::Window,
         Event::User,
-        Event::Asset>
+        Event::Asset,
+        Event::TaskComplete>
         data;
     template <typename T>
     const auto& Get() const
@@ -190,99 +199,3 @@ struct Event {
         return std::get<T>(data);
     }
 };
-
-inline bool operator<(const Event::Empty& a, const Event::Empty& b)
-{
-    return false;
-}
-
-inline bool operator<(const Event::Keyboard& a, const Event::Keyboard& b)
-{
-    return a.window < b.window
-        && a.key < b.key
-        && a.state < b.state
-        && a.repeat < b.repeat
-        && a.alt < b.alt
-        && a.control < b.control
-        && a.shift < b.shift;
-}
-
-inline bool operator<(const Event::TextInput& a, const Event::TextInput& b)
-{
-    return a.text < b.text
-        && a.window < b.window;
-}
-
-inline bool operator<(const Event::TextEdit& a, const Event::TextEdit& b)
-{
-    return a.length < b.length
-        && a.start < b.start
-        && a.text < b.text
-        && a.window < b.window;
-}
-
-#include <glm/vec2.hpp>
-#include <glm/vector_relational.hpp>
-
-inline bool operator<(const Event::MouseMove& a, const Event::MouseMove& b)
-{
-    return glm::all(glm::lessThan(a.position, b.position))
-        && glm::all(glm::lessThan(a.relative, b.relative))
-        && a.window < b.window;
-}
-
-inline bool operator<(const Event::MouseButton& a, const Event::MouseButton& b)
-{
-    return a.button < b.button
-        && glm::all(glm::lessThan(a.position, b.position))
-        && a.window < b.window;
-}
-
-inline bool operator<(const Event::MouseWheel& a, const Event::MouseWheel& b)
-{
-    return glm::all(glm::lessThan(a.amount, b.amount))
-        && a.direction < b.direction
-        && glm::all(glm::lessThan(a.position, b.position))
-        && a.window < b.window;
-}
-
-inline bool operator<(const Event::GameControllerDevice& a, const Event::GameControllerDevice& b)
-{
-    return a.id < b.id;
-}
-
-inline bool operator<(const Event::GameControllerAxis& a, const Event::GameControllerAxis& b)
-{
-    return a.axis < b.axis
-        && a.id < b.id
-        && a.value < b.value;
-}
-
-inline bool operator<(const Event::GameControllerButton& a, const Event::GameControllerButton& b)
-{
-    return a.button < b.button
-        && a.id < b.id
-        && a.state < b.state;
-}
-
-inline bool operator<(const Event::Window& a, const Event::Window& b)
-{
-    return a.type < b.type
-        && a.window < b.window;
-}
-
-inline bool operator<(const Event::User& a, const Event::User& b)
-{
-    return a.data < b.data;
-}
-
-inline bool operator<(const Event::Asset& a, const Event::Asset& b)
-{
-    return a.asset < b.asset;
-}
-
-inline bool operator<(const Event& a, const Event& b)
-{
-    return a.type < b.type
-        && a.data < b.data;
-}
