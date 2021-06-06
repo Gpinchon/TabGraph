1176a865ff719071bb33d287c44b19bb0f1d4704
gpinchon <gpinchon@student.42.fr>
2021-06-07 01:31:23 +0200

    proper Texture2D compression & syntax

--- TabGraph/src/Driver/OpenGL/Texture/Texture2D.cpp
+++ TabGraph/src/Driver/OpenGL/Texture/Texture2D.cpp
@@ -2,21 +2,21 @@
 * @Author: gpinchon
 * @Date:   2021-05-02 20:50:11
 * @Last Modified by:   gpinchon
-* @Last Modified time: 2021-05-02 20:50:11
+* @Last Modified time: 2021-06-07 01:30:06
 */
 
-#include <Driver/OpenGL/Texture/Texture2D.hpp>
-#include <Driver/OpenGL/Texture/PixelUtils.hpp>
 #include <Assets/Asset.hpp>
 #include <Assets/AssetsParser.hpp>
 #include <Assets/Image.hpp>
-#include <Event/EventsManager.hpp>
 #include <DispatchQueue.hpp>
+#include <Driver/OpenGL/Texture/PixelUtils.hpp>
+#include <Driver/OpenGL/Texture/Texture2D.hpp>
+#include <Event/EventsManager.hpp>
 
 #include <FasTC/CompressedImage.h>
+#include <FasTC/CompressionFormat.h>
 #include <FasTC/Image.h>
 #include <FasTC/TexComp.h>
-#include <FasTC/CompressionFormat.h>
 #include <GL/glew.h>
 
 Texture2D::Impl::Impl(const Impl& other)
@@ -29,7 +29,7 @@ Texture2D::Impl::Impl(const Impl& other)
 }
 
 Texture2D::Impl::Impl(const glm::ivec2& size, const Pixel::Description& pixelDesc)
-	: Texture::Impl(Texture::Type::Texture2D, pixelDesc)
+    : Texture::Impl(Texture::Type::Texture2D, pixelDesc)
     , _size(size)
 {
 }
@@ -42,30 +42,27 @@ Texture2D::Impl::Impl(std::shared_ptr<Asset> asset)
 
 void Texture2D::Impl::Load()
 {
-	if (GetLoaded() ||
-        _imageLoadingSlot.Connected() ||
-        _imageCompressionSlot.Connected())
+    if (GetLoaded() || _imageLoadingSlot.Connected() || _imageCompressionSlot.Connected())
         return;
-    auto asset{ GetImage() };
+    auto asset { GetImage() };
     if (asset == nullptr) {
         //We don't have an image to load from, just allocate on GPU
         _AllocateStorage();
         SetLoaded(true);
         return;
     }
-    auto assetLoaded{ asset->GetLoaded() };
+    auto assetLoaded { asset->GetLoaded() };
     if (assetLoaded) {
         _UploadImage();
         return;
     }
-    AssetsParser::AddParsingTask({
-        AssetsParser::ParsingTask::Type::Async,
-        asset
-    });
+    AssetsParser::AddParsingTask({ AssetsParser::ParsingTask::Type::Async,
+        asset });
     _imageLoadingSlot = EventsManager::On(Event::Type::AssetLoaded).Connect([this](const Event& event) {
         assert(!_loaded);
         auto& assetEvent = event.Get<Event::Asset>();
-        if (assetEvent.asset != GetImage()) return;
+        if (assetEvent.asset != GetImage())
+            return;
         _UploadImage();
         _imageLoadingSlot.Disconnect();
     });
@@ -89,10 +86,12 @@ void Texture2D::Impl::GenerateMipmap()
 
 void Texture2D::Impl::SetSize(const glm::ivec2& size)
 {
-    if (GetSize() == size) return;
+    if (GetSize() == size)
+        return;
     _size = size;
     Unload();
-    if (GetAutoMipMap()) SetMipMapNbr(MIPMAPNBR(GetSize()));
+    if (GetAutoMipMap())
+        SetMipMapNbr(MIPMAPNBR(GetSize()));
 }
 
 void Texture2D::Impl::SetCompressed(bool compressed)
@@ -103,36 +102,29 @@ void Texture2D::Impl::SetCompressed(bool compressed)
     Unload();
 }
 
-void Texture2D::Impl::_AllocateStorage() {
+void Texture2D::Impl::_AllocateStorage()
+{
     _handle = OpenGL::Texture::Generate();
     Bind();
-    if (GetCompressed()) {
-        glTexStorage2D(
-            OpenGL::GetEnum(GetType()),
-            GetMipMapNbr(),
-            GL_COMPRESSED_RGBA_S3TC_DXT5_EXT,
-            GetSize().x, GetSize().y
-        );
-    }
-    else
-        glTexStorage2D(
-            OpenGL::GetEnum(GetType()),
-            GetMipMapNbr(),
-            OpenGL::GetEnum(GetPixelDescription().GetSizedFormat()),
-            GetSize().x, GetSize().y
-        );
+    glTexStorage2D(
+        OpenGL::GetEnum(GetType()),
+        GetMipMapNbr(),
+        OpenGL::GetEnum(GetPixelDescription().GetSizedFormat()),
+        GetSize().x, GetSize().y);
     Done();
 }
 
-inline void Texture2D::Impl::_UploadImage() {
-    auto image{ GetImage()->GetComponent<Image>() };
+inline void Texture2D::Impl::_UploadImage()
+{
+    auto image { GetImage()->GetComponent<Image>() };
     assert(image != nullptr);
-    _pixelDescription = image->GetPixelDescription();
+
     SetSize(image->GetSize());
     if (GetCompressed() && !_imageCompressionSlot.Connected()) {
+        _pixelDescription = Pixel::SizedFormat::DXT5_RGBA;
         _AllocateStorage();
         _imageCompressionTaskID = DispatchQueue::ApplicationDispatchQueue().Dispatch([this] {
-            auto image{ GetImage()->GetComponent<Image>() };
+            auto image { GetImage()->GetComponent<Image>() };
             _imageCompressionBuffer.resize(CompressedImage::GetCompressedSize(
                 image->GetSize().x, image->GetSize().y,
                 FasTC::ECompressionFormat::eCompressionFormat_DXT5));
@@ -147,15 +139,13 @@ inline void Texture2D::Impl::_UploadImage() {
                 image->GetSize().x, image->GetSize().y,
                 (unsigned char*)_imageCompressionBuffer.data(),
                 _imageCompressionBuffer.size(),
-                compressionSetting
-            );
+                compressionSetting);
         });
         _imageCompressionSlot = EventsManager::On(Event::Type::TaskComplete).Connect([this](const Event& event) {
-            auto &taskEvent{ event.Get<Event::TaskComplete>() };
-            if (taskEvent.taskID != _imageCompressionTaskID.taskID ||
-                taskEvent.dispatchQueueID != _imageCompressionTaskID.dispatchQueueID)
+            auto& taskEvent { event.Get<Event::TaskComplete>() };
+            if (taskEvent.taskID != _imageCompressionTaskID.taskID || taskEvent.dispatchQueueID != _imageCompressionTaskID.dispatchQueueID)
                 return;
-            auto image{ GetImage()->GetComponent<Image>() };
+            auto image { GetImage()->GetComponent<Image>() };
             Bind();
             glCompressedTexSubImage2D(
                 OpenGL::GetEnum(Texture::Type::Texture2D),
@@ -163,8 +153,7 @@ inline void Texture2D::Impl::_UploadImage() {
                 image->GetSize().x, image->GetSize().y,
                 GL_COMPRESSED_RGBA_S3TC_DXT5_EXT,
                 _imageCompressionBuffer.size(),
-                _imageCompressionBuffer.data()
-            );
+                _imageCompressionBuffer.data());
             Done();
             _imageCompressionBuffer.clear();
             _imageCompressionBuffer.shrink_to_fit();
@@ -174,8 +163,8 @@ inline void Texture2D::Impl::_UploadImage() {
             SetLoaded(true);
             _imageCompressionSlot.Disconnect();
         });
-    }
-    else {
+    } else {
+        _pixelDescription = image->GetPixelDescription();
         _AllocateStorage();
         Bind();
         glTexSubImage2D(
@@ -187,8 +176,7 @@ inline void Texture2D::Impl::_UploadImage() {
             image->GetSize().y,
             OpenGL::GetEnum(image->GetPixelDescription().GetUnsizedFormat()),
             OpenGL::GetEnum(image->GetPixelDescription().GetType()),
-            image->GetData().data()
-        );
+            image->GetData().data());
         Done();
         if (GetAutoMipMap())
             GenerateMipmap();
