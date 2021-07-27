e6fad23bb6da3912f1149e6ac6d3045875691649
Guillaume <pinchon.guillaume@gmail.com>
2021-07-27 21:43:19 +0200

    add missing Light file

--- TabGraph/include/Light/SkyLight.hpp
+++ TabGraph/include/Light/SkyLight.hpp
@@ -9,59 +9,53 @@
 // Includes
 ////////////////////////////////////////////////////////////////////////////////
 #include <Light/DirectionalLight.hpp>
+#include <Core/Inherit.hpp>
+#include <Core/Property.hpp>
 #include <SphericalHarmonics.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
 // Class declarations
 ////////////////////////////////////////////////////////////////////////////////
 namespace TabGraph::Lights {
-class SkyLight : public DirectionalLight {
+class SkyLight : public Core::Inherit<DirectionalLight, SkyLight> {
+    READONLYPROPERTY(float, SunPower, 20);
+    READONLYPROPERTY(float, PlanetRadius, 6360e3);
+    READONLYPROPERTY(float, AtmosphereRadius, 6420e3);
+    READONLYPROPERTY(float, HRayleigh, 7994);
+    READONLYPROPERTY(float, HMie, 1200);
+    READONLYPROPERTY(glm::vec3, BetaRayleigh, 5.5e-6, 13.0e-6, 22.4e-6);
+    READONLYPROPERTY(glm::vec3, BetaMie, 21e-6);
+
 public:
     SkyLight();
     glm::vec3 GetSunDirection() const;
     void SetSunDirection(const glm::vec3& dir);
-    float GetSunPower() const;
     void SetSunPower(const float power);
     /**
-     * @return Planet's radius in meters
+     * @arg radius : Planet's radius in meters
     */
-    float GetPlanetRadius() const;
     void SetPlanetRadius(float radius);
     /**
-     * @return Planet's radius in meters
+     * @arg radius : Planet's atmosphere radius in meters
     */
-    float GetAtmosphereRadius() const;
     void SetAtmosphereRadius(float radius);
     /**
-     * @return Rayleigh scale heigh
+     * @arg hRayleigh : Rayleigh scale heigh
     */
-    float GetHRayleigh() const;
     void SetHRayleigh(float hRayleigh);
     /**
-     * @return Mie scale heigh
+     * @arg hMie : Mie scale heigh
     */
-    float GetHMie() const;
     void SetHMie(float hMie);
     /**
-     * @return Rayleigh's scattering coefficient at sea level
+     * @arg betaRayleigh : Rayleigh's scattering coefficient at sea level
     */
-    glm::vec3 GetBetaRayleigh() const;
     void SetBetaRayleigh(glm::vec3 betaRayleigh);
     /**
-     * @return Mie's scattering coefficient at sea level
+     * @arg betaMie : Mie's scattering coefficient at sea level
     */
-    glm::vec3 GetBetaMie() const;
     void SetBetaMie(glm::vec3 betaMie);
 
     glm::vec3 GetIncidentLight(glm::vec3 direction) const;
-
-private:
-    float _sunPower { 20.f };
-    float _planetRadius { 6360e3 };
-    float _atmosphereRadius { 6420e3 };
-    float _hRayleigh { 7994 };
-    float _hMie { 1200 };
-    glm::vec3 _betaRayleigh { 5.5e-6, 13.0e-6, 22.4e-6 };
-    glm::vec3 _betaMie { 21e-6 };
 };
 }
