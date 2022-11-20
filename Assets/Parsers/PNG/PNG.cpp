3f9eceed5af1fbde4498bf1ffbce8eafb2aeb949
gpinchon <pinchon.guillaume@gmail.com>
2022-11-21 00:29:07 +0100

    add PNG parser

---
+++ Assets/Parsers/PNG/PNG.cpp
@@ -0,0 +1,154 @@
+#include <Assets/Asset.hpp>
+#include <SG/Image/Image.hpp>
+#include <SG/Buffer/Buffer.hpp>
+#include <SG/Buffer/View.hpp>
+#include <Tools/Debug.hpp>
+
+#include <png.h>
+
+#include <iostream>
+#include <fstream>
+
+namespace TabGraph::Assets {
+namespace PNG {
+struct Struct {
+	Struct(png_structp a_Ptr) : _ptr(a_Ptr) {}
+	operator png_structp () const { return _ptr; }
+	operator bool() const { return _ptr != nullptr; }
+
+protected:
+	png_structp _ptr;
+};
+struct Read : Struct {
+	Read() : Struct(png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL)) {}
+	~Read() { png_destroy_read_struct(&_ptr, nullptr, nullptr); }
+};
+struct Write : Struct {
+	Write() : Struct(png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL)) {}
+	~Write() { png_destroy_write_struct(&_ptr, nullptr); }
+
+};
+struct Info {
+	Info(const Struct& a_PngRead)
+		: _read(a_PngRead)
+		, _ptr(png_create_info_struct(_read))
+	{}
+	~Info() { png_destroy_info_struct(_read, &_ptr); }
+	operator png_infop () const { return _ptr; }
+	operator bool() const { return _ptr != nullptr; }
+
+private:
+	const Struct& _read;
+	png_infop _ptr;
+};
+void ReadData(png_structp pngPtr, png_bytep data, png_size_t length) {
+	//Here we get our IO pointer back from the read struct.
+	//This is the parameter we passed to the png_set_read_fn() function.
+	//Our std::istream pointer.
+	png_voidp a = png_get_io_ptr(pngPtr);
+	//Cast the pointer to std::istream* and read 'length' bytes into 'data'
+	((std::istream*)a)->read((char*)data, length);
+}
+constexpr auto SIGSIZE = 8;
+bool Validate(std::istream& source) {
+	png_byte header[SIGSIZE]{};
+	source.read((char*)header, SIGSIZE);
+	if (png_sig_cmp(header, 0, SIGSIZE)) {
+		errorLog("png_sig_cmp failed");
+		return false;
+	}
+
+	return true;
+}
+
+auto GetSizedFormat(uint8_t a_Channels, uint8_t a_ByteDepth) {
+	SG::Pixel::UnsizedFormat format = SG::Pixel::UnsizedFormat::Unknown;
+	switch (a_Channels) {
+	case 1:
+		format = SG::Pixel::UnsizedFormat::R;
+	case 2:
+		format = SG::Pixel::UnsizedFormat::RG;
+	case 3:
+		format = SG::Pixel::UnsizedFormat::RGB;
+	case 4:
+		format = SG::Pixel::UnsizedFormat::RGBA;
+	}
+	SG::Pixel::Type type = SG::Pixel::Type::Unknown;
+	switch (a_ByteDepth) {
+	case 8:
+		type = SG::Pixel::Type::Uint8;
+	case 16:
+		type = SG::Pixel::Type::Uint16;
+	}
+	return SG::Pixel::GetSizedFormat(format, type, true);
+}
+}
+
+std::shared_ptr<Asset> ParsePNG(const std::shared_ptr<Asset>& a_Container)
+{
+	auto path = a_Container->GetUri().DecodePath();
+	std::cout << path << std::endl;
+	std::ifstream file(path, std::ios_base::binary);
+	if (!PNG::Validate(file)) return a_Container;
+	PNG::Read pngRead;
+	if (!pngRead) {
+		errorLog("png_create_read_struct failed");
+		return a_Container;
+	}
+	PNG::Info pngInfo(pngRead);
+	if (!pngInfo) {
+		errorLog("png_create_info_struct failed");
+		return a_Container;
+	}
+	if (setjmp(png_jmpbuf(pngRead))) {
+		//An error occured, so clean up what we have allocated so far...
+		errorLog("Error while reading PNG");
+		//Make sure you return here. libPNG will jump to here if something
+		//goes wrong, and if you continue with your normal code, you might
+		//End up with an infinite loop.
+		return a_Container; // Do your own error handling here.
+	}
+	png_set_sig_bytes(pngRead, PNG::SIGSIZE);
+	png_set_read_fn(pngRead, (png_voidp)&file, PNG::ReadData);
+	png_read_info(pngRead, pngInfo);//Now call png_read_info with our pngPtr as image handle, and infoPtr to receive the file info.
+	auto width    = png_get_image_width(pngRead, pngInfo);
+	auto height   = png_get_image_height(pngRead, pngInfo);
+	auto bitDepth = png_get_bit_depth(pngRead, pngInfo);
+	auto channels = png_get_channels(pngRead, pngInfo);
+	auto colorT	  = png_get_color_type(pngRead, pngInfo);
+	switch (colorT) {
+	case PNG_COLOR_TYPE_PALETTE:
+		png_set_palette_to_rgb(pngRead);
+		channels = 3;
+		break;
+	case PNG_COLOR_TYPE_GRAY:
+		if (bitDepth < 8)
+			png_set_expand_gray_1_2_4_to_8(pngRead);
+		bitDepth = 8;
+		break;
+	}
+	if (png_get_valid(pngRead, pngInfo, PNG_INFO_tRNS)) {
+		png_set_tRNS_to_alpha(pngRead);
+		channels += 1;
+	}
+	if (bitDepth == 16)
+		png_set_strip_16(pngRead);
+	png_read_update_info(pngRead, pngInfo);
+	std::vector<std::byte*> rows(height, nullptr);
+	auto data = std::make_shared<SG::Buffer>(width * height * bitDepth * channels / 8);
+	const auto stride = width * bitDepth * channels / 8;
+	for (size_t i = 0; i < height; i++) {
+		auto q = (height - i - 1) * stride;
+		rows.at(i) = &data->at(q);
+	}
+	png_read_image(pngRead, (png_bytepp)rows.data());
+
+	auto image = std::make_shared<SG::Image>();
+	image->SetType(SG::Image::Type::Image2D);
+	image->SetPixelDescription(PNG::GetSizedFormat(channels, bitDepth));
+	image->SetSize({ width, height, 1 });
+	image->SetBufferView(std::make_shared<SG::BufferView>(data, 0, data->size()));
+	a_Container->assets.push_back(image);
+	return a_Container;
+}
+}
