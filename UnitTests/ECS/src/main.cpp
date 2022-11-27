b21865bc02b6f91d56cada7549c144c18a98a2d6
gpinchon <pinchon.guillaume@gmail.com>
2022-11-27 17:59:47 +0100

    fix ECS test

--- UnitTests/ECS/src/main.cpp
+++ UnitTests/ECS/src/main.cpp
@@ -73,26 +73,35 @@ auto TestECS(const Registry& a_Registry) {
             auto entity = a_Registry->CreateEntity();
             entity.AddComponent<Transform>();
             if (i % 2) entity.AddComponent<Name>();
-        }
-    }
-    {
-        Tools::ScopedTimer timer("Deleting entities");
-        for (auto i = 0u; i < entities.size(); ++i) {
-            if (i % 3) entities.at(i) = {};
+            entities.push_back(entity);
         }
     }
     auto view = a_Registry->GetView<Transform, Name>();
     {
         Tools::ScopedTimer timer("Updating positions");
-        view.ForEach<Transform>([a_Registry](auto entity, Transform& transform) {
+        view.ForEach<Transform>([a_Registry](auto entity, auto& transform) {
             transform.position.x = entity;
         });
+    }
+    {
+        Tools::ScopedTimer timer("Checking components");
+        size_t entityCount = 0;
+        view.ForEach<Name>([a_Registry, &entityCount](auto entity, auto& name) {
+            entityCount++;
+        });
+        assert(entityCount == 30000);
     }
     {
         Tools::ScopedTimer timer("Checking positions");
         view.ForEach<Transform>([a_Registry](auto entity, Transform& transform) {
             assert(transform.position.x == entity);
         });
+    }
+    {
+        Tools::ScopedTimer timer("Deleting entities");
+        for (auto i = 0u; i < entities.size(); ++i) {
+            if (i % 3) entities.at(i) = {};
+        }
     }
     {
         Tools::ScopedTimer timer("Clearing remaining entities");
