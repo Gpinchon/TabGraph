c73adb8976b61adfd06f38860c542d489f11446c
gpinchon <pinchon.guillaume@gmail.com>
2022-11-28 01:25:43 +0100

    constness correction & simplify code

--- ECS/include/ECS/View.hpp
+++ ECS/include/ECS/View.hpp
@@ -18,21 +18,21 @@ public:
     * the entities with the specified components
     */
     template<typename... Args>
-    void ForEach(const std::function<void(typename RegistryType::EntityIDType, Args&...)>& a_Func);
+    void ForEach(const std::function<void(typename RegistryType::EntityIDType, Args&...)>& a_Func) const;
 
 private:
     friend RegistryType;
     View(RegistryType*, Types&&... a_Types);
     template<typename T>
-    auto& _GetStorage();
+    auto& _GetStorage() const;
     template <typename... Args>
-    auto _Get();
-    std::tuple<Types...> _storage;
+    auto _Get() const;
+    const std::tuple<Types...> _storage;
 };
 template<typename RegistryType, typename ...Types>
 template<typename ...Args>
-inline void View<RegistryType, Types...>::ForEach(const std::function<void(typename RegistryType::EntityIDType, Args&...)>& a_Func) {
-    auto storages = _Get<Args...>();
+inline void View<RegistryType, Types...>::ForEach(const std::function<void(typename RegistryType::EntityIDType, Args&...)>& a_Func) const {
+    auto& storages = _Get<Args...>();
     typename RegistryType::EntityIDType firstEntity{ RegistryType::MaxEntities }, lastEntity{ 0 };
     std::apply([&firstEntity, &lastEntity](const auto& a_Storage) {
         firstEntity = std::min(a_Storage.FirstEntity(), firstEntity);
@@ -43,18 +43,18 @@ inline void View<RegistryType, Types...>::ForEach(const std::function<void(typen
         std::apply([&firstEntity, &hasComponents](auto& a_Storage) {
             if (!a_Storage.HasComponent(firstEntity)) hasComponents = false;
         }, storages);
-        if (hasComponents) a_Func(firstEntity, std::ref(_GetStorage<Args>().get().Get(firstEntity))...);
+        if (hasComponents) a_Func(firstEntity, _GetStorage<Args>().get().Get(firstEntity)...);
         ++firstEntity;
     }
 }
 template<typename RegistryType, typename ...Types>
 template<typename T>
-inline auto& View<RegistryType, Types...>::_GetStorage() {
+inline auto& View<RegistryType, Types...>::_GetStorage() const {
     return std::get<std::reference_wrapper<ComponentTypeStorage<T, RegistryType>>>(_storage);
 }
 template<typename RegistryType, typename ...Types>
 template<typename ...Args>
-inline auto View<RegistryType, Types...>::_Get() {
+inline auto View<RegistryType, Types...>::_Get() const {
     return std::make_tuple(std::get<std::reference_wrapper<ComponentTypeStorage<Args, RegistryType>>>(_storage)...);
 }
 template<typename RegistryType, typename ...Types>
