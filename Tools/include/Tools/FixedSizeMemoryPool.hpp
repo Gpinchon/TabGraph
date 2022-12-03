8dfdfbb0617d8aafbde084453da1b82524146098
gpinchon <pinchon.guillaume@gmail.com>
2022-12-03 14:28:45 +0100

    cleanup FixedSizeMemoryPool

--- Tools/include/Tools/FixedSizeMemoryPool.hpp
+++ Tools/include/Tools/FixedSizeMemoryPool.hpp
@@ -16,82 +16,43 @@ public:
     typedef Type value_type;
     typedef uint32_t size_type;
     typedef ptrdiff_t difference_type;
-
     static_assert(sizeof(value_type) >= sizeof(size_type));
 
+    template<typename U> struct rebind { typedef FixedSizeMemoryPool<U, Size> other; };
     class Deleter {
     public:
-        Deleter(FixedSizeMemoryPool& a_Pool) : _memoryPool(a_Pool) {}
-        void operator()(Type* const a_Ptr) { _memoryPool.deallocate(a_Ptr); }
+        constexpr Deleter(FixedSizeMemoryPool& a_Pool) : _memoryPool(a_Pool) {}
+        constexpr void operator()(Type* const a_Ptr) { _memoryPool.deallocate(a_Ptr); }
 
     private:
         FixedSizeMemoryPool& _memoryPool;
     };
 
-    template<typename U> struct rebind { typedef FixedSizeMemoryPool<U, Size> other; };
-
-    FixedSizeMemoryPool() {
-        std::memset(_memory, 0, sizeof(_memory));
-        *reinterpret_cast<size_type*>(_next) = 0;
-    }
-    FixedSizeMemoryPool(FixedSizeMemoryPool&& a_Other)
-        : _cellNumUsed(std::move(a_Other._cellNumUsed))
-        , _cellNumFree(std::move(a_Other._cellNumFree))
-        , _memory(std::move(a_Other._memory))
-        , _next(std::move(a_Other._next))
-    {}
-    FixedSizeMemoryPool(const FixedSizeMemoryPool& a_Other) noexcept : FixedSizeMemoryPool() {}
+    inline constexpr FixedSizeMemoryPool() noexcept;
+    inline constexpr FixedSizeMemoryPool(FixedSizeMemoryPool&& a_Other) noexcept;
+    inline constexpr FixedSizeMemoryPool(const FixedSizeMemoryPool& a_Other) noexcept : FixedSizeMemoryPool() {}
     template<typename U>
-    FixedSizeMemoryPool(const FixedSizeMemoryPool<U, Size>&) noexcept : FixedSizeMemoryPool() {}
+    inline constexpr FixedSizeMemoryPool(const FixedSizeMemoryPool<U, Size>&) noexcept : FixedSizeMemoryPool() {}
 
-    Type* allocate() {
-        if (_cellNumUsed < max_size()) {
-            auto p = (size_type*)addr_from_index(_cellNumUsed);
-            *p = ++_cellNumUsed;
-        }
-        Type* res = nullptr;
-        if (_cellNumFree > 0)
-        {
-            res = (Type*)_next;
-            if (--_cellNumFree > 0)
-                _next = addr_from_index(*(uint32_t*)_next);
-            else _next = nullptr;
-        }
-        return res;
-    }
-    void deallocate(Type* const a_Ptr) noexcept {
-        if (_next != nullptr) {
-            *(size_type*)a_Ptr = index_from_addr(_next);
-            _next = (std::byte*)a_Ptr;
-        }
-        else {
-            *(size_type*)a_Ptr = max_size();
-            _next = (std::byte*)a_Ptr;
-        }
-        ++_cellNumFree;
-    }
+    /** @return the pointer to a newly allocated object */
+    inline constexpr value_type* allocate() noexcept;
+    /** @brief returns the space of the pointer to the pool */
+    inline constexpr void deallocate(value_type* const a_Ptr) noexcept;
 
-    bool empty() const {
-        return _cellNumFree == max_size();
-    }
-    size_type index_from_addr(std::byte* a_Ptr) const {
-        return size_type(a_Ptr - _memory) / sizeof(value_type);
-    }
-    std::byte* addr_from_index(size_type a_Index) {
-        return _memory + (a_Index * sizeof(value_type));
-    }
-    auto deleter() noexcept {
-        return Deleter(*this);
-    }
-    size_type count() const noexcept {
-        return max_size() - _cellNumFree;
-    }
-    size_type free() const noexcept {
-        return _cellNumFree;
-    }
-    constexpr size_type max_size() const noexcept {
-        return Size;
-    }
+    /** @return true if this pool contains no object */
+    inline constexpr bool empty() const noexcept;
+    /** @return the count of objects contained in this pool */
+    inline constexpr size_type count() const noexcept;
+    /** @return the number of available slots in this pool */
+    inline constexpr size_type free() const noexcept;
+    /** @return the maximum number of object this pool can contain */
+    inline constexpr size_type max_size() const noexcept;
+
+    /** @return a deleter constructed from this pool */
+    inline constexpr Deleter deleter() noexcept;
+
+    inline constexpr size_type index_from_addr(std::byte* a_Ptr) const noexcept;
+    inline constexpr std::byte* addr_from_index(size_type a_Index)  const noexcept;
 
     template<typename U>
     bool operator!=(const FixedSizeMemoryPool<U, Size>& a_Right) { return false; }
@@ -104,4 +65,83 @@ private:
     std::byte*              _next{ &_memory[0] };
     alignas(value_type) std::byte _memory[sizeof(value_type) * Size]{};
 };
+
+template<typename Type, uint32_t Size>
+inline constexpr FixedSizeMemoryPool<Type, Size>::FixedSizeMemoryPool() noexcept {
+    std::memset(_memory, 0, sizeof(_memory));
+    *reinterpret_cast<size_type*>(_next) = 0;
+}
+
+template<typename Type, uint32_t Size>
+inline constexpr FixedSizeMemoryPool<Type, Size>::FixedSizeMemoryPool(FixedSizeMemoryPool&& a_Other) noexcept
+    : _cellNumUsed(std::move(a_Other._cellNumUsed))
+    , _cellNumFree(std::move(a_Other._cellNumFree))
+    , _memory(std::move(a_Other._memory))
+    , _next(std::move(a_Other._next))
+{}
+
+template<typename Type, uint32_t Size>
+inline constexpr auto FixedSizeMemoryPool<Type, Size>::allocate() noexcept -> value_type* {
+    if (_cellNumUsed < max_size()) {
+        auto p = (size_type*)addr_from_index(_cellNumUsed);
+        *p = ++_cellNumUsed;
+    }
+    Type* res = nullptr;
+    if (_cellNumFree > 0)
+    {
+        res = (Type*)_next;
+        if (--_cellNumFree > 0)
+            _next = addr_from_index(*(uint32_t*)_next);
+        else _next = nullptr;
+    }
+    return res;
+}
+
+template<typename Type, uint32_t Size>
+inline constexpr void FixedSizeMemoryPool<Type, Size>::deallocate(value_type* const a_Ptr) noexcept {
+    if (_next != nullptr) {
+        *(size_type*)a_Ptr = index_from_addr(_next);
+        _next = (std::byte*)a_Ptr;
+    }
+    else {
+        *(size_type*)a_Ptr = max_size();
+        _next = (std::byte*)a_Ptr;
+    }
+    ++_cellNumFree;
+}
+
+template<typename Type, uint32_t Size>
+inline constexpr bool FixedSizeMemoryPool<Type, Size>::empty() const noexcept {
+    return _cellNumFree == max_size();
+}
+
+template<typename Type, uint32_t Size>
+inline constexpr auto FixedSizeMemoryPool<Type, Size>::index_from_addr(std::byte* a_Ptr) const noexcept -> size_type {
+    return size_type(a_Ptr - _memory) / sizeof(value_type);
+}
+
+template<typename Type, uint32_t Size>
+inline constexpr std::byte* FixedSizeMemoryPool<Type, Size>::addr_from_index(size_type a_Index) const noexcept {
+    return _memory + (a_Index * sizeof(value_type));
+}
+
+template<typename Type, uint32_t Size>
+inline constexpr auto FixedSizeMemoryPool<Type, Size>::deleter() noexcept -> Deleter {
+    return Deleter(*this);
+}
+
+template<typename Type, uint32_t Size>
+inline constexpr auto FixedSizeMemoryPool<Type, Size>::count() const noexcept -> size_type {
+    return max_size() - _cellNumFree;
+}
+
+template<typename Type, uint32_t Size>
+inline constexpr auto FixedSizeMemoryPool<Type, Size>::free() const noexcept -> size_type {
+    return _cellNumFree;
+}
+
+template<typename Type, uint32_t Size>
+inline constexpr auto FixedSizeMemoryPool<Type, Size>::max_size() const noexcept -> size_type {
+    return Size;
+}
 }
