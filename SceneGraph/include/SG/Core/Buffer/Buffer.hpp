fe315b91271d7287d01ef4e7e61d818fb66c7cb8
gpinchon <pinchon.guillaume@gmail.com>
2022-12-15 23:48:56 +0100

    add constructor for Buffer

--- SceneGraph/include/SG/Core/Buffer/Buffer.hpp
+++ SceneGraph/include/SG/Core/Buffer/Buffer.hpp
@@ -33,6 +33,7 @@ public:
     inline Buffer(const std::string& a_Name) : Buffer() { SetName(a_Name); }
     inline Buffer(const size_t& a_Size) : Buffer() { resize(a_Size); }
     inline Buffer(const std::vector<std::byte>& a_RawData) : Buffer() { SetRawData(a_RawData); }
+    inline Buffer(const std::byte* a_RawDataPtr, const size_t a_DataByteSize) : Buffer() { SetRawData({ a_RawDataPtr, a_RawDataPtr + a_DataByteSize }); }
     inline void resize(const size_t& a_Size) { GetRawData().resize(a_Size); }
     inline auto size() { return GetRawData().size(); }
     inline auto data() { return GetRawData().data(); }
