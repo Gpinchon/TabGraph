247bdccf028f022a67b2e38c082fece47c7a77ce
gpinchon <pinchon.guillaume@gmail.com>
2023-07-01 13:20:12 +0200

    allow stb parsing from data uri

--- Assets/Parsers/STBImage/STBImage.cpp
+++ Assets/Parsers/STBImage/STBImage.cpp
@@ -7,13 +7,12 @@
 #include <stb_image.h>
 
 #include <fstream>
+#include <strstream>
 #include <memory>
 
 namespace TabGraph::Assets {
-std::shared_ptr<Asset> ParseSTBImage(const std::shared_ptr<Asset>& a_Container)
+std::shared_ptr<Asset> ParseSTBFromStream(const std::shared_ptr<Asset>& a_Container, std::istream& a_Stream)
 {
-    auto path = a_Container->GetUri().DecodePath();
-    std::ifstream file(path, std::ios_base::binary);
     int width = 0, height = 0, comp = 0;
     stbi_io_callbacks cb {};
     cb.read = [](void* a_User, char* a_Data, int a_Size) -> int {
@@ -26,9 +25,7 @@ std::shared_ptr<Asset> ParseSTBImage(const std::shared_ptr<Asset>& a_Container)
     cb.eof = [](void* a_User) -> int {
         return ((std::istream*)a_User)->peek() == EOF;
     };
-    auto string = path.u8string();
-    // auto bytes = stbi_load(string.c_str(), &width, &height, &comp, 0);
-    auto bytes = stbi_load_from_callbacks(&cb, &file, &width, &height, &comp, 0);
+    auto bytes = stbi_load_from_callbacks(&cb, &a_Stream, &width, &height, &comp, 0);
     auto buffer = std::make_shared<SG::Buffer>(std::vector<std::byte>((std::byte*)bytes, (std::byte*)bytes + (width * height * comp)));
     stbi_image_free(bytes);
     auto image = std::make_shared<SG::Image>();
@@ -51,6 +48,29 @@ std::shared_ptr<Asset> ParseSTBImage(const std::shared_ptr<Asset>& a_Container)
     image->SetType(SG::Image::Type::Image2D);
     a_Container->AddObject(image);
     a_Container->SetLoaded(true);
+    return a_Container;
+}
+
+std::shared_ptr<Asset> ParseSTBFromFile(const std::shared_ptr<Asset>& a_Container)
+{
+    std::ifstream stream(a_Container->GetUri().DecodePath(), std::ios_base::binary);
+    return ParseSTBFromStream(a_Container, stream);
+}
+
+std::shared_ptr<Asset> ParseSTBFromBinary(const std::shared_ptr<Asset>& a_Container)
+{
+    auto binary = DataUri(a_Container->GetUri()).Decode();
+    auto stream = std::istrstream(reinterpret_cast<const char*>(binary.data()), binary.size());
+    return ParseSTBFromStream(a_Container, stream);
+}
+
+std::shared_ptr<Asset> ParseSTBImage(const std::shared_ptr<Asset>& a_Container)
+{
+    auto& uri = a_Container->GetUri();
+    if (uri.GetScheme() == "data")
+        return ParseSTBFromBinary(a_Container);
+    else
+        return ParseSTBFromFile(a_Container);
     return a_Container;
 }
 
