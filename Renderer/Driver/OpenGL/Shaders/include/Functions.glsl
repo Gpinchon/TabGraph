7d5f7af8a8ffa57ad733e732e4c02897ad553aa4
gpinchon <gpinchon@student.42.fr>
2024-06-12 18:48:49 +0200

    glsl-cpp WIP

--- Renderer/Driver/OpenGL/Shaders/include/Functions.glsl
+++ Renderer/Driver/OpenGL/Shaders/include/Functions.glsl
@@ -2,19 +2,34 @@
 #define FUNCTIONS
 
 #ifdef __cplusplus
+
+#include <Types.glsl>
 #include <glm/common.hpp>
 #include <glm/integer.hpp>
-#define IN(type)    const type&
-#define OUT(type)   type&
-#define INOUT(type) type&
+
 namespace TabGraph::Renderer::GLSL {
 constexpr auto fract           = glm::fract;
-constexpr auto bitfieldReverse = glm::bitfieldReverse
+constexpr auto all             = glm::all;
+constexpr auto dot             = glm::dot;
+constexpr auto bitfieldReverse = glm::bitfieldReverse;
+constexpr auto lessThanEqual   = glm::lessThanEqual;
 }
+
+#define IN(type)               const type&
+#define OUT(type)              type&
+#define INOUT(type)            type&
+#define M_PI                   3.1415926535897932384626433832795
+#define PI                     M_PI
+#define EPSILON                0.0001
+#define lequal(a, b)           all(lessThanEqual(a, b))
+#define Luminance(linearColor) dot(linearColor, vec3(0.299, 0.587, 0.114))
+
 #else //__cplusplus
+
 #define IN(type)    in type
 #define OUT(type)   out type
 #define INOUT(type) inout type
+
 #endif //__cplusplus
 
 #endif // FUNCTIONS
