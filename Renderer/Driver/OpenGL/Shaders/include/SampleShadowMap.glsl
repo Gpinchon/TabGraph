94509b5c8726940023ed276debcd35aca1749306
gpinchon <gpinchon@student.42.fr>
2024-06-12 19:03:06 +0200

    SampleShadowMap doc

--- Renderer/Driver/OpenGL/Shaders/include/SampleShadowMap.glsl
+++ Renderer/Driver/OpenGL/Shaders/include/SampleShadowMap.glsl
@@ -18,20 +18,27 @@
 #define SHADOW_SAMPLES 9
 #endif
 
+/**
+ * @arg a_Bias : the bias used to try and avoid shadow acnee
+ * @arg a_Projection : the shadowmap projection matrix
+ * @arg a_WorldPosition : the current fragment's world position
+ * @arg a_RandBase : the base used for shadow bluring, best value : ivec3(gl_FragCoord.xy, FrameNumber % 8)
+ * @arg a_ShadowTexture : the shadow map
+ */
 float SampleShadowMap(
-    IN(float) bias,
-    IN(mat4) projection,
-    IN(vec3) worldPosition,
-    IN(vec2) randBase,
-    sampler2DShadow shadowTexture)
+    IN(float) a_Bias,
+    IN(mat4) a_Projection,
+    IN(vec3) a_WorldPosition,
+    IN(ivec3) a_RandBase,
+    sampler2DShadow a_ShadowTexture)
 {
-    const vec4 shadowPos = projection * vec4(worldPosition, 1.0);
+    const vec4 shadowPos = a_Projection * vec4(a_WorldPosition, 1.0);
     const vec3 projCoord = vec3(shadowPos.xyz / shadowPos.w) * 0.5 + 0.5;
-    const uvec2 Random   = Rand3DPCG16(ivec3(randBase, FrameNumber % 8)).xy;
+    const uvec2 random   = Rand3DPCG16(a_RandBase).xy;
     float shadow         = 0;
     for (int i = 0; i < SHADOW_SAMPLES; i++) {
-        vec2 sampleUV = projCoord.xy + Hammersley(i, SHADOW_SAMPLES, Random) * SHADOWBLURRADIUS;
-        shadow += texture(shadowTexture, vec3(sampleUV, projCoord.z - bias));
+        vec2 sampleUV = projCoord.xy + Hammersley(i, SHADOW_SAMPLES, random) * SHADOWBLURRADIUS;
+        shadow += texture(a_ShadowTexture, vec3(sampleUV, projCoord.z - a_Bias));
     }
     return (shadow / float(SHADOW_SAMPLES));
 }
