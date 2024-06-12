e268c58a860459daff18d27bc2e2041f318c724b
gpinchon <gpinchon@student.42.fr>
2024-06-12 18:47:54 +0200

    add defines to shader compiler

--- Renderer/Driver/OpenGL/include/Renderer/OGL/ShaderCompiler.hpp
+++ Renderer/Driver/OpenGL/include/Renderer/OGL/ShaderCompiler.hpp
@@ -5,6 +5,7 @@
 #include <Tools/ObjectCache.hpp>
 
 #include <string>
+#include <unordered_map>
 
 namespace TabGraph::Renderer {
 struct Context;
@@ -16,11 +17,13 @@ struct Shader;
 
 namespace TabGraph::Renderer {
 using ShaderCacheKey = Tools::ObjectCacheKey<unsigned, std::string>;
+using ShaderDefines  = std::unordered_map<std::string, std::string>;
 struct ShaderCompiler : private Tools::ObjectCache<ShaderCacheKey, std::shared_ptr<RAII::Shader>> {
     ShaderCompiler(Context& a_Context);
     RAII::Shader& CompileShader(
         unsigned a_Stage,
-        const std::string& a_Code);
+        const std::string& a_Code,
+        const ShaderDefines& a_Defines = {});
     Context& context;
 };
 }
--- Renderer/Driver/OpenGL/src/Renderer/OGL/ShaderCompiler.cpp
+++ Renderer/Driver/OpenGL/src/Renderer/OGL/ShaderCompiler.cpp
@@ -1,6 +1,8 @@
 #include <Renderer/OGL/RAII/Shader.hpp>
 #include <Renderer/OGL/ShaderCompiler.hpp>
 
+#include <Renderer/ShaderPreprocessor.hpp>
+
 #include <Tools/LazyConstructor.hpp>
 
 #include <regex>
@@ -12,10 +14,16 @@ ShaderCompiler::ShaderCompiler(Context& a_Context)
 }
 RAII::Shader& ShaderCompiler::CompileShader(
     unsigned a_Stage,
-    const std::string& a_Code)
+    const std::string& a_Code,
+    const ShaderDefines& a_Defines)
 {
-    auto lazyConstructor = Tools::LazyConstructor([this, a_Stage, a_Code] {
-        return RAII::MakePtr<RAII::Shader>(context, a_Stage, a_Code.data());
+    std::string finalCode;
+    for (auto& define : a_Defines) {
+        finalCode += "#define " + define.first + " " + define.second + ";\n";
+    }
+    finalCode += a_Code;
+    auto lazyConstructor = Tools::LazyConstructor([this, a_Stage, finalCode] {
+        return RAII::MakePtr<RAII::Shader>(context, a_Stage, ShaderPreprocessor {}.ExpandCode(finalCode).data());
     });
     return *GetOrCreate(a_Stage, a_Code, lazyConstructor);
 }
