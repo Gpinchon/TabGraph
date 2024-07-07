d3f9f886310f9deabb8c1c10444687a8d0a7572e
gpinchon <gpinchon@student.42.fr>
2024-07-08 00:06:22 +0200

    use proper defines for vtfs inputs

--- Renderer/Driver/OpenGL/Shaders/GLSL/header/VTFSInputs.glsl
+++ Renderer/Driver/OpenGL/Shaders/GLSL/header/VTFSInputs.glsl
@@ -1,29 +1,25 @@
 #ifndef VTFS_INPUTS_GLSL
 #define VTFS_INPUTS_GLSL
-
-#define VTFS_LIGHT_BUFFER_SSBO 0
-#define VTFS_CLUSTERS_SSBO     1
-
 #ifndef __cplusplus
+#include <Bindings.glsl>
 #include <VTFS.glsl>
 
-layout(std430, binding = VTFS_LIGHT_BUFFER_SSBO) readonly buffer VTFSLightsBufferSSBO
+layout(std430, binding = SSBO_VTFS_LIGHTS) readonly buffer VTFSLightsBufferSSBO
 {
     LightBase lightBase[VTFS_BUFFER_MAX];
 };
-layout(std430, binding = VTFS_LIGHT_BUFFER_SSBO) readonly buffer VTFSLightSpotBufferSSBO
+layout(std430, binding = SSBO_VTFS_LIGHTS) readonly buffer VTFSLightSpotBufferSSBO
 {
     LightSpot lightSpot[VTFS_BUFFER_MAX];
 };
-layout(std430, binding = VTFS_LIGHT_BUFFER_SSBO) readonly buffer VTFSLightDirBufferSSBO
+layout(std430, binding = SSBO_VTFS_LIGHTS) readonly buffer VTFSLightDirBufferSSBO
 {
     LightDirectional lightDirectional[VTFS_BUFFER_MAX];
 };
 
-layout(std430, binding = VTFS_CLUSTERS_SSBO) readonly buffer VTFSClustersSSBO
+layout(std430, binding = SSBO_VTFS_CLUSTERS) readonly buffer VTFSClustersSSBO
 {
     VTFSCluster vtfsClusters[];
 };
 #endif //__cplusplus
-
 #endif // VTFS_INPUTS_GLSL
