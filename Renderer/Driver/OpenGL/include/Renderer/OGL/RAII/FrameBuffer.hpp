0fc0c9020f13e4468141a3af64c3d7dd1f23b964
gpinchon <gpinchon@student.42.fr>
2024-07-17 19:29:26 +0200

    [Rendering KO] update framebuffer creation

--- Renderer/Driver/OpenGL/include/Renderer/OGL/RAII/FrameBuffer.hpp
+++ Renderer/Driver/OpenGL/include/Renderer/OGL/RAII/FrameBuffer.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
+#include <GL/glew.h>
+#include <glm/vec3.hpp>
+
 namespace TabGraph::Renderer::RAII {
+struct Texture2D;
+}
+
+namespace TabGraph::Renderer::RAII {
+struct FrameBufferAttachment {
+    GLenum attachment;
+    std::shared_ptr<RAII::Texture2D> texture;
+};
+struct FrameBufferCreateInfo {
+    glm::uvec3 defaultSize { -1, -1, -1 };
+    std::vector<FrameBufferAttachment> colorBuffers;
+    std::shared_ptr<RAII::Texture2D> depthBuffer;
+    std::shared_ptr<RAII::Texture2D> stencilBuffer;
+};
 struct FrameBuffer {
-    FrameBuffer(const unsigned& a_Width, const unsigned& a_Height);
+    FrameBuffer(const FrameBufferCreateInfo& a_Info);
     ~FrameBuffer();
     operator unsigned() const { return handle; }
     const unsigned handle = 0;
-    const unsigned width = -1, height = -1;
+    const FrameBufferCreateInfo info;
 };
 }
--- Renderer/Driver/OpenGL/src/Renderer/OGL/RAII/FrameBuffer.cpp
+++ Renderer/Driver/OpenGL/src/Renderer/OGL/RAII/FrameBuffer.cpp
@@ -12,13 +12,29 @@ auto CreateFramebuffer()
     return handle;
 }
 
-FrameBuffer::FrameBuffer(const unsigned& a_Width, const unsigned& a_Height)
+FrameBuffer::FrameBuffer(const FrameBufferCreateInfo& a_Info)
     : handle(CreateFramebuffer())
-    , width(a_Width)
-    , height(a_Height)
+    , info(a_Info)
 {
-    glNamedFramebufferParameteri(handle, GL_FRAMEBUFFER_DEFAULT_WIDTH, width);
-    glNamedFramebufferParameteri(handle, GL_FRAMEBUFFER_DEFAULT_HEIGHT, height);
+    glNamedFramebufferParameteri(handle, GL_FRAMEBUFFER_DEFAULT_WIDTH, info.defaultSize.x);
+    glNamedFramebufferParameteri(handle, GL_FRAMEBUFFER_DEFAULT_HEIGHT, info.defaultSize.y);
+    glNamedFramebufferParameteri(handle, GL_FRAMEBUFFER_DEFAULT_LAYERS, info.defaultSize.z);
+    for (const auto& colorBuffer : info.colorBuffers) {
+        glNamedFramebufferTexture(
+            handle, colorBuffer.attachment,
+            *colorBuffer.texture, 0);
+    }
+    if (info.depthBuffer != nullptr) {
+        glNamedFramebufferTexture(
+            handle, GL_DEPTH_ATTACHMENT,
+            *info.depthBuffer, 0);
+    }
+    if (info.stencilBuffer != nullptr) {
+        glNamedFramebufferTexture(
+            handle, GL_STENCIL_ATTACHMENT,
+            *info.stencilBuffer, 0);
+    }
+    assert(glCheckNamedFramebufferStatus(handle, GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
 }
 
 FrameBuffer::~FrameBuffer()
