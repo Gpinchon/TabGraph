ec57662b644bef92dd7377c7e04f3dd0b7568254
gpinchon <gpinchon@student.42.fr>
2024-07-19 00:44:34 +0200

    add TODO

--- Renderer/Driver/OpenGL/include/Renderer/OGL/Texture2DPool.hpp
+++ Renderer/Driver/OpenGL/include/Renderer/OGL/Texture2DPool.hpp
@@ -14,6 +14,7 @@
 #include <glm/glm.hpp>
 
 // TODO stop using Power of two and use texture views on 8k textures
+// TODO use a quadtree to handle texture atlas subdivision
 
 namespace TabGraph::Renderer {
 struct Context;
