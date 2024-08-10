016376585b13252ccbd45c1e652e7870dbb04e62
gpinchon <gpinchon@student.42.fr>
2024-08-10 15:26:05 +0200

    fix BRDF integration

--- Tools/include/Tools/BRDFIntegration.hpp
+++ Tools/include/Tools/BRDFIntegration.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
 
 namespace TabGraph::Tools::BRDFIntegration {
 using Color  = glm::vec2;
@@ -11,5 +12,11 @@ enum class Type {
     Standard,
     Sheen
 };
+/// @brief
+/// @param a_Xi random vector (usually Halton23 but could also be Hammersley)
+/// @param a_N normal vector
+/// @param a_Roughness perceptial roughness
+/// @return
+glm::vec3 ImportanceSampleGGX(const glm::vec2& a_Xi, const glm::vec3& a_N, const float& a_Roughness);
 Pixels Generate(unsigned a_Width, unsigned a_Height, Type a_Type);
 }
--- Tools/src/Tools/BRDFIntegration.cpp
+++ Tools/src/Tools/BRDFIntegration.cpp
@@ -1,4 +1,5 @@
 #include <Tools/BRDFIntegration.hpp>
+#include <Tools/Halton.hpp>
 
 #include <glm/glm.hpp>
 
@@ -9,36 +10,41 @@
  */
 
 namespace TabGraph::Tools::BRDFIntegration {
-constexpr auto sampleCount = 16;
-float RadicalInverse_VdC(unsigned int bits)
+glm::vec3 ImportanceSampleGGX(const glm::vec2& a_Xi, const glm::vec3& a_N, const float& a_Roughness)
 {
-    bits = (bits << 16u) | (bits >> 16u);
-    bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
-    bits = ((bits & 0x33333333u) << 2u) | ((bits & 0xCCCCCCCCu) >> 2u);
-    bits = ((bits & 0x0F0F0F0Fu) << 4u) | ((bits & 0xF0F0F0F0u) >> 4u);
-    bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);
-    return float(bits) * 2.3283064365386963e-10;
+    const float a        = a_Roughness * a_Roughness;
+    const float Phi      = 2 * M_PI * a_Xi.x;
+    const float CosTheta = sqrt((1.f - a_Xi.y) / (1 + (a * a - 1) * a_Xi.y));
+    const float SinTheta = sqrt(1.f - CosTheta * CosTheta);
+    const glm::vec3 H    = {
+        SinTheta * cos(Phi),
+        SinTheta * sin(Phi),
+        CosTheta
+    };
+
+    const glm::vec3 UpVector = abs(a_N.z) < 0.999 ? glm::vec3(0, 0, 1) : glm::vec3(1, 0, 0);
+    const glm::vec3 TangentX = glm::normalize(glm::cross(UpVector, a_N));
+    const glm::vec3 TangentY = glm::cross(a_N, TangentX);
+
+    return glm::normalize(TangentX * H.x + TangentY * H.y + a_N * H.z);
 }
 
-glm::vec2 Hammersley(unsigned int i, unsigned int N)
+float GeometrySchlickGGX(float NdotV, float roughness)
 {
-    return glm::vec2(float(i) / float(N), RadicalInverse_VdC(i));
-}
+    float a = roughness;
+    float k = (a * a) / 2.0;
 
-float G1_GGX_Schlick(float NdotV, float roughness)
-{
-    float r = roughness; // original
-    // float r = 0.5 + 0.5 * roughness; // Disney remapping
-    float k     = (r * r) / 2.0;
+    float nom   = NdotV;
     float denom = NdotV * (1.0 - k) + k;
-    return NdotV / denom;
+
+    return nom / denom;
 }
 
-float G_Smith(float NdotV, float NoL, float roughness)
+float GeometrySmith(float NdotV, float NdotL, float roughness)
 {
-    float g1_l = G1_GGX_Schlick(NoL, roughness);
-    float g1_v = G1_GGX_Schlick(NdotV, roughness);
-    return g1_l * g1_v;
+    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+    return ggx1 * ggx2;
 }
 
 float l(float x, float alpha_g)
@@ -57,53 +63,58 @@ float lambda_sheen(float cos_theta, float alpha_g)
     return abs(cos_theta) < 0.5 ? exp(l(cos_theta, alpha_g)) : exp(2.0 * l(0.5, alpha_g) - l(1.0 - cos_theta, alpha_g));
 }
 
+template <unsigned Size>
+glm::vec2 Halton23(const unsigned& a_Index)
+{
+    constexpr auto halton2 = Tools::Halton<2>::Sequence<Size>();
+    constexpr auto halton3 = Tools::Halton<3>::Sequence<Size>();
+    const auto rIndex      = a_Index % Size;
+    return { halton2[rIndex], halton3[rIndex] };
+}
+
+template <size_t Samples>
 glm::vec2 IntegrateBRDF(float roughness, float NdotV, Type a_Type)
 {
-    glm::vec3 V;
-    V.x              = sqrt(1.0 - NdotV * NdotV); // sin
-    V.y              = 0.0;
-    V.z              = NdotV; // cos
+    const glm::vec3 V(
+        sqrt(1.0 - NdotV * NdotV),
+        0.f,
+        NdotV);
+    const glm::vec3 N(0.0, 0.0, 1.0);
     glm::vec2 result = { 0, 0 };
-    for (uint n = 1u; n <= sampleCount; n++) {
-        glm::vec2 p = Hammersley(n, sampleCount);
-        float a     = roughness * roughness;
-        float theta = acos(sqrt((1.0 - p.y) / (1.0 + (a * a - 1.0) * p.y)));
-        float phi   = 2.0 * M_PI * p.x;
-        // sampled h direction in normal space
-        glm::vec3 H = glm::vec3(sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta));
-        glm::vec3 L = 2.0f * dot(V, H) * H - V;
+    for (uint n = 0u; n < Samples; n++) {
+        const auto Xi = Halton23<Samples>(n);
+        const auto H  = ImportanceSampleGGX(Xi, N, roughness);
+        const auto L  = glm::normalize(2.f * dot(V, H) * H - V);
 
-        // because N = vec3(0.0, 0.0, 1.0) follows
-        float NdotL = glm::clamp(L.z, 0.0f, 1.0f);
-        float NdotH = glm::clamp(H.z, 0.0f, 1.0f);
-        float VdotH = glm::clamp(dot(V, H), 0.0f, 1.0f);
-        if (NdotL > 0.0) {
-            float G_Vis = 0;
-            if (a_Type == Type::Standard) {
-                const float G = G_Smith(NdotV, NdotL, roughness);
-                G_Vis         = G * VdotH / (NdotH * NdotV);
-            } else if (a_Type == Type::Sheen) {
-                G_Vis = 1.0 / ((1.0 + lambda_sheen(NdotV, roughness) + lambda_sheen(NdotL, roughness)) * (4.0 * NdotV * NdotL));
-            }
-            float Fc = pow(1.0 - VdotH, 5.0);
-            result.x += (1.0 - Fc) * G_Vis;
-            result.y += Fc * G_Vis;
+        float NdotL = glm::max(L.z, 0.f);
+        float NdotH = glm::max(H.z, 0.f);
+        float VdotH = glm::max(dot(V, H), 0.f);
+        if (NdotL <= 0.0)
+            continue;
+        float Fc    = pow(1.0 - VdotH, 5.0);
+        float G_Vis = 0;
+        if (a_Type == Type::Standard) {
+            float G = GeometrySmith(NdotV, NdotL, roughness);
+            G_Vis   = (G * VdotH) / (NdotH * NdotV);
+        } else if (a_Type == Type::Sheen) {
+            G_Vis = 1.0 / ((1.0 + lambda_sheen(NdotV, roughness) + lambda_sheen(NdotL, roughness)) * (4.0 * NdotV * NdotL));
         }
+
+        result += glm::vec2(
+            (1.0 - Fc) * G_Vis,
+            Fc * G_Vis);
     }
-    return result / float(sampleCount);
+    return result / float(Samples);
 }
 
 Pixels Generate(unsigned a_Width, unsigned a_Height, Type a_Type)
 {
     Pixels pixels(a_Width, std::vector<Color>(a_Height));
     for (auto y = 0; y < a_Height; ++y) {
-        float yCoord = (y + 0.5f) / float(a_Width);
+        const float roughness = y / float(a_Height - 1);
         for (auto x = 0; x < a_Width; ++x) {
-            float xCoord       = (x + 0.5f) / float(a_Height);
-            float NdotV        = xCoord;
-            float roughness    = yCoord;
-            const auto brdfVal = IntegrateBRDF(roughness, NdotV, a_Type);
-            pixels[x][y]       = brdfVal;
+            const float NdotV = (x + 1) / float(a_Width);
+            pixels[x][y]      = IntegrateBRDF<64>(roughness, NdotV, a_Type);
         }
     }
     return pixels;
