cca7c31535d2a9154eef93196ab6500096f9191e
Gpinchon <pinchon.guillaume@gmail.com>
2024-08-26 13:17:25 +0200

    fix deprecation warnings

--- Renderer/Driver/OpenGL/include/Renderer/OGL/RAII/Wrapper.hpp
+++ Renderer/Driver/OpenGL/include/Renderer/OGL/RAII/Wrapper.hpp
@@ -14,14 +14,14 @@ template <typename Type, typename... Args>
 std::shared_ptr<Type> MakePtr(Context& a_Context, Args&&... a_Args)
 {
     std::pmr::polymorphic_allocator<Type> al(&a_Context.memoryResource);
-    Type* ptr = al.allocate(1);
+    Type* ptr = std::allocator_traits<decltype(al)>::allocate(al, 1);
     a_Context.PushImmediateCmd([ptr, &a_Args...]() { new (ptr) Type(std::forward<Args>(a_Args)...); }, true);
     return {
         ptr, [&context = a_Context](Type* a_Ptr) mutable {
             context.PushCmd([ptr = a_Ptr, mr = &context.memoryResource] {
                 std::pmr::polymorphic_allocator<Type> al(mr);
-                al.destroy(ptr);
-                al.deallocate(ptr, 1);
+                std::allocator_traits<decltype(al)>::destroy(al, ptr);
+                std::allocator_traits<decltype(al)>::deallocate(al, ptr, 1);
             });
         }
     };
