95e44fca3fbb03d4dd5932a6f60631f68c4ccace
gpinchon <pinchon.guillaume@gmail.com>
2024-09-06 04:17:59 +0200

    fix & improve BMP parser

--- Assets/Parsers/BMP/BMP.cpp
+++ Assets/Parsers/BMP/BMP.cpp
@@ -10,9 +10,11 @@
 #include <SG/Core/Buffer/View.hpp>
 #include <SG/Core/Image/Image2D.hpp>
 
+#include <glm/glm.hpp> // for s_vec2, glm::vec2
+
+#include <algorithm>
 #include <errno.h> // for errno
 #include <fcntl.h> // for O_BINARY, O_CREAT, O_RDWR
-#include <glm/glm.hpp> // for s_vec2, glm::vec2
 #include <stdexcept> // for runtime_error
 #include <stdio.h> // for fclose, fread, fopen, fseek, SEE...
 #include <string.h> // for memset, strerror
@@ -35,47 +37,128 @@
 #endif
 
 namespace TabGraph::Assets {
+enum class Compression {
+    RGB       = 0x0000,
+    RLE8      = 0x0001,
+    RLE4      = 0x0002,
+    BITFIELDS = 0x0003,
+    JPEG      = 0x0004,
+    PNG       = 0x0005,
+    CMYK      = 0x000B,
+    CMYKRLE8  = 0x000C,
+    CMYKRLE4  = 0x000D
+};
 #pragma pack(1)
+struct t_bmp_pixel_24 {
+    union {
+        struct {
+            uint8_t red;
+            uint8_t green;
+            uint8_t blue;
+        };
+        unsigned bitSet : 24;
+    };
+};
+
+struct t_bmp_pixel_32 {
+    union {
+        struct {
+            uint8_t red;
+            uint8_t green;
+            uint8_t blue;
+            uint8_t alpha;
+        };
+        unsigned bitSet : 32;
+    };
+};
+
+struct t_bmp_gamma {
+    uint32_t red;
+    uint32_t green;
+    uint32_t blue;
+};
+struct t_bmp_cie_xyz {
+    int32_t x = 0;
+    int32_t y = 0;
+    int32_t z = 0;
+};
+struct t_bmp_cie_xyz_triple {
+    t_bmp_cie_xyz red;
+    t_bmp_cie_xyz green;
+    t_bmp_cie_xyz blue;
+};
+struct t_bmp_color_mask {
+    t_bmp_color_mask(
+        uint8_t a_Red,
+        uint8_t a_Green,
+        uint8_t a_Blue,
+        uint8_t a_Alpha)
+        : red(a_Red)
+        , green(a_Green)
+        , blue(a_Blue)
+        , alpha(a_Alpha)
+    {
+    }
+    union {
+        struct {
+            uint8_t red;
+            uint8_t green;
+            uint8_t blue;
+            uint8_t alpha;
+        };
+        uint32_t bitSet;
+    };
+};
+
 /// \private
 struct t_bmp_info {
-    uint32_t header_size { sizeof(t_bmp_info) };
-    int32_t width { 0 };
-    int32_t height { 0 };
-    uint16_t color_planes { 0 };
-    uint16_t bpp { 0 };
-    uint32_t compression_method { 0 };
-    uint32_t size { 0 };
-    int32_t horizontal_resolution { 0 };
-    int32_t vertical_resolution { 0 };
-    uint32_t totalColors { 0 };
-    uint32_t important_colors { 0 };
+    int32_t width                  = 0;
+    int32_t height                 = 0;
+    uint16_t color_planes          = 0;
+    uint16_t bpp                   = 0;
+    Compression compression_method = Compression::RGB;
+    uint32_t size                  = 0;
+    int32_t horizontal_resolution  = 0;
+    int32_t vertical_resolution    = 0;
+    uint32_t colors_used           = 0;
+    uint32_t important_colors      = 0;
+    t_bmp_color_mask red_mask      = { 1, 0, 0, 0 };
+    t_bmp_color_mask green_mask    = { 0, 1, 0, 0 };
+    t_bmp_color_mask blue_mask     = { 0, 0, 1, 0 };
+    t_bmp_color_mask alpha_mask    = { 0, 0, 0, 1 };
+    uint32_t color_space_type      = 0;
+    t_bmp_cie_xyz_triple cie_xyz   = {};
+    t_bmp_gamma gamma              = {};
+    uint32_t intent                = 0;
+    uint32_t profile_data          = 0;
+    uint32_t profile_size          = 0;
+    uint32_t reserved              = 0;
 };
 
 /// \private
 struct t_bmp_header {
-    std::byte type[2] { std::byte(0), std::byte(0) };
-    uint32_t size { 0 };
-    std::byte reserved1[2] { std::byte(0), std::byte(0) };
-    std::byte reserved2[2] { std::byte(0), std::byte(0) };
-    uint32_t data_offset { sizeof(t_bmp_header) + sizeof(t_bmp_info) };
+    uint16_t type        = 0x4D42;
+    uint32_t size        = 0;
+    uint16_t reserved1   = 0;
+    uint16_t reserved2   = 0;
+    uint32_t data_offset = 0;
 };
 #pragma pack()
 
 /// \private
 struct t_bmp_parser {
-    FILE* fd { nullptr };
-    t_bmp_info info;
-    t_bmp_header header;
-    std::vector<std::byte> data;
-    unsigned size_read { 0 };
+    FILE* fd                    = nullptr;
+    t_bmp_info info             = {};
+    t_bmp_header header         = {};
+    std::vector<std::byte> data = {};
+    unsigned size_read          = 0;
 };
 
 using namespace TabGraph;
 
 static void prepare_header(t_bmp_header* header, t_bmp_info* info, std::shared_ptr<SG::Image> t)
 {
-    header->type[0]             = std::byte(0x42);
-    header->type[1]             = std::byte(0x4D);
+    header->type                = 0x424D;
     header->size                = header->data_offset + (t->GetSize().x * t->GetSize().y * 4);
     info->width                 = t->GetSize().x;
     info->height                = t->GetSize().y;
@@ -119,32 +202,69 @@ void SaveBMP(std::shared_ptr<SG::Image> image, const std::string& imagepath)
     close(fd);
 }
 
-static void convert_bmp(t_bmp_parser* parser)
+static void convert_bmp_to_rgb(t_bmp_parser* parser)
 {
-    std::vector<std::byte> pixel_temp { parser->data.size() };
-    std::byte rgba[4] { std::byte(0), std::byte(0), std::byte(0), std::byte(0) };
-    int i[3] { 0, -1, 0 };
-
-    while (++i[1] < parser->info.width) {
-        i[2] = -1;
-        while (++i[2] < parser->info.height) {
-            rgba[0]                                   = parser->data.at(static_cast<size_t>(i[0]) + 1);
-            rgba[1]                                   = parser->data.at(static_cast<size_t>(i[0]) + 2);
-            rgba[2]                                   = parser->data.at(static_cast<size_t>(i[0]) + 3);
-            rgba[3]                                   = parser->data.at(static_cast<size_t>(i[0]) + 0);
-            pixel_temp[static_cast<size_t>(i[0]) + 0] = rgba[0];
-            pixel_temp[static_cast<size_t>(i[0]) + 1] = rgba[1];
-            pixel_temp[static_cast<size_t>(i[0]) + 2] = rgba[2];
-            pixel_temp[static_cast<size_t>(i[0]) + 3] = rgba[3];
-            i[0] += (parser->info.bpp / 8);
+    for (size_t i = 0; i < parser->data.size(); i += parser->info.bpp / 8) {
+        if (parser->info.bpp == 24) {
+            t_bmp_pixel_24* ptr  = reinterpret_cast<t_bmp_pixel_24*>(&parser->data.at(i));
+            t_bmp_pixel_24 color = *ptr;
+            ptr->red             = color.blue;
+            ptr->green           = color.green;
+            ptr->blue            = color.red;
+        } else if (parser->info.bpp == 32) {
+            t_bmp_pixel_32* ptr  = reinterpret_cast<t_bmp_pixel_32*>(&parser->data.at(i));
+            t_bmp_pixel_32 color = *ptr;
+            ptr->red             = color.alpha;
+            ptr->green           = color.blue;
+            ptr->blue            = color.green;
+            ptr->alpha           = color.red;
         }
     }
-    parser->data = pixel_temp;
+}
+
+static void convert_bitfield_bmp(t_bmp_parser* parser)
+{
+    auto redMask   = parser->info.red_mask.bitSet;
+    auto greenMask = parser->info.green_mask.bitSet;
+    auto blueMask  = parser->info.blue_mask.bitSet;
+    auto alphaMask = parser->info.alpha_mask.bitSet;
+    for (size_t i = 0; i < parser->data.size(); i += parser->info.bpp / 8) {
+        if (parser->info.bpp == 24) {
+            t_bmp_pixel_24* ptr  = reinterpret_cast<t_bmp_pixel_24*>(&parser->data.at(i));
+            t_bmp_pixel_24 color = *ptr;
+            auto red             = color.bitSet & redMask;
+            auto green           = color.bitSet & greenMask;
+            auto blue            = color.bitSet & blueMask;
+            ptr->bitSet          = red | green | blue;
+        } else if (parser->info.bpp == 32) {
+            t_bmp_pixel_32* ptr  = reinterpret_cast<t_bmp_pixel_32*>(&parser->data.at(i));
+            t_bmp_pixel_32 color = *ptr;
+            auto red             = color.bitSet & redMask;
+            auto green           = color.bitSet & greenMask;
+            auto blue            = color.bitSet & blueMask;
+            auto alpha           = color.bitSet & alphaMask;
+            ptr->bitSet          = red | green | blue | alpha;
+        }
+    }
+}
+
+static void flip_bmp(t_bmp_parser* parser)
+{
+    std::array<std::byte, 4096> pixel_temp;
+    auto lineSize = parser->info.width * (parser->info.bpp / 8);
+    for (auto y = 0, y1 = parser->info.height - 1; y < parser->info.height / 2; y++, y1--) {
+        auto topLineIndex    = y * lineSize;
+        auto bottomLineIndex = y1 * lineSize;
+        std::swap_ranges(
+            &parser->data.at(topLineIndex), &parser->data.at(topLineIndex + lineSize),
+            &parser->data.at(bottomLineIndex));
+    }
 }
 
 static int read_data(t_bmp_parser* p, const std::filesystem::path& path)
 {
     unsigned data_size;
+    bool needsVerticalFlip = true;
 
     if (access(path.string().c_str(), R_OK) != 0) {
         throw std::runtime_error(strerror(errno));
@@ -153,26 +273,38 @@ static int read_data(t_bmp_parser* p, const std::filesystem::path& path)
         throw std::runtime_error(strerror(errno));
     }
     auto readReturn = fread(&p->header, 1, sizeof(p->header), p->fd);
-    if (readReturn != sizeof(p->header) || p->header.type[0] != std::byte(0x42) || p->header.type[1] != std::byte(0x4D)) {
+    if (readReturn != sizeof(p->header) || p->header.type != 0x4D42) {
         fclose(p->fd);
         throw std::runtime_error("Wrong Header");
     }
-    readReturn = fread(&p->info, 1, sizeof(p->info), p->fd);
-    if (readReturn != sizeof(p->info)) {
+    uint32_t bmp_info_size = 0;
+    readReturn             = fread(&bmp_info_size, 1, sizeof(bmp_info_size), p->fd);
+    if (readReturn != sizeof(bmp_info_size)) {
         fclose(p->fd);
         throw std::runtime_error("Wrong Info");
     }
-    if (p->info.width * p->info.bpp / 8 % 2)
-        data_size = 4 * p->info.width * p->info.height;
-    else
+    readReturn = fread(&p->info, 1, bmp_info_size, p->fd);
+    if (readReturn != bmp_info_size) {
+        fclose(p->fd);
+        throw std::runtime_error("Wrong Info");
+    }
+    if (p->info.height < 0) {
+        needsVerticalFlip = false;
+        p->info.height    = std::abs(p->info.height);
+    }
+    if (p->info.compression_method == Compression::RGB)
         data_size = p->info.bpp / 8 * p->info.width * p->info.height;
+    else
+        data_size = p->info.size;
     fseek(p->fd, p->header.data_offset, SEEK_SET);
     p->data.resize(data_size);
     p->size_read = fread(p->data.data(), sizeof(std::byte), data_size, p->fd);
     fclose(p->fd);
-    if (p->info.bpp == 32) {
-        convert_bmp(p);
-    }
+    if (p->info.compression_method == Compression::BITFIELDS)
+        convert_bitfield_bmp(p);
+    convert_bmp_to_rgb(p);
+    if (needsVerticalFlip)
+        flip_bmp(p);
     return (0);
 }
 
@@ -199,12 +331,13 @@ std::shared_ptr<Asset> ParseBMP(const std::shared_ptr<Asset>& asset)
     } catch (std::exception& e) {
         throw std::runtime_error(std::string("Error parsing ") + asset->GetUri().DecodePath().string() + " : " + e.what());
     }
-    auto format { GetBMPPixelFormat(parser.info.bpp) };
     auto buffer = std::make_shared<SG::Buffer>(parser.data);
-    auto image  = std::make_shared<SG::Image2D>(format, parser.info.width, parser.info.height, std::make_shared<SG::BufferView>(buffer, 0, buffer->size()));
+    auto image  = std::make_shared<SG::Image2D>(
+        GetBMPPixelFormat(parser.info.bpp),
+        size_t(parser.info.width), size_t(parser.info.height),
+        std::make_shared<SG::BufferView>(buffer, 0, buffer->size()));
     asset->AddObject(image);
     asset->SetLoaded(true);
     return asset;
 }
-
 }
