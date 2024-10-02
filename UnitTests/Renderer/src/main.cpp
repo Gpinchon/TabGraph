f30b3d2da5397de1ecb760699d3fd0be5ac7bd92
gpinchon <pinchon.guillaume@gmail.com>
2024-10-02 14:43:32 +0200

    increase nbr of cubes & lights in Renderer unit test because we can

--- UnitTests/Renderer/src/main.cpp
+++ UnitTests/Renderer/src/main.cpp
@@ -278,9 +278,9 @@ struct TabGraphWindow {
 
 constexpr auto testWindowWidth  = 1280;
 constexpr auto testWindowHeight = 800;
-constexpr auto testGridSize     = 20;
-constexpr auto testCubesNbr     = 10;
-constexpr auto testLightNbr     = 10;
+constexpr auto testGridSize     = 100;
+constexpr auto testCubesNbr     = 50;
+constexpr auto testLightNbr     = 50;
 
 auto GetCameraProj(const uint32_t& a_Width, const uint32_t& a_Height)
 {
